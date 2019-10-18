98c2ebe005388aa2efec9f1f5aaea168eda63750
InfinityGhost <seelkadoom36@gmail.com>
2019-10-17 21:26:58 -0400

    Add unhandled exception message box

--- OpenTabletDriverGUI/Program.cs
+++ OpenTabletDriverGUI/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Avalonia;
 using Avalonia.Logging.Serilog;
+using MessageBox.Avalonia;
+using MessageBox.Avalonia.DTO;
 using OpenTabletDriverGUI.ViewModels;
 using OpenTabletDriverGUI.Views;
 
@@ -24,6 +26,8 @@ namespace OpenTabletDriverGUI
         // container, etc.
         private static void AppMain(Application app, string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(UnhandledException);
+
             var viewModel = new MainWindowViewModel();
             viewModel.Initialize();
             var window = new MainWindow
@@ -32,6 +36,20 @@ namespace OpenTabletDriverGUI
             };
 
             app.Run(window);
+        }
+
+        private static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var exception = (Exception)e.ExceptionObject ?? default;
+            var msgbox = MessageBoxWindow.CreateCustomWindow(new MessageBoxCustomParams
+            {
+                ContentTitle = "Fatal Exception",
+                ContentHeader = exception.GetType().FullName,
+                ContentMessage = exception.ToString(),
+                ShowInCenter = true,
+                CanResize = false,
+            });
+            msgbox.Show();
         }
     }
 }
