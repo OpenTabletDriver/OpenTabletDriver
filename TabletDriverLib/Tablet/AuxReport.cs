2cade7339ab7bced2390c1ad52f18b8fc7a14517
InfinityGhost <seelkadoom36@gmail.com>
2019-12-12 22:52:37 -0500

    Add aux button indicators

--- TabletDriverLib/Tablet/AuxReport.cs
+++ TabletDriverLib/Tablet/AuxReport.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TabletDriverLib.Tablet
 {
     public struct AuxReport : IAuxReport
@@ -5,11 +7,26 @@ namespace TabletDriverLib.Tablet
         public AuxReport(byte[] report)
         {
             Raw = report;
-            // TODO: Add aux button indicators
-            AuxButtons = null;
+            AuxButtons = new bool[]
+            {
+                (report[3] & (1 << 0)) != 0,
+                (report[3] & (1 << 1)) != 0,
+                (report[3] & (1 << 2)) != 0,
+                (report[3] & (1 << 3)) != 0
+            };
         }
 
         public byte[] Raw { private set; get; }
         public bool[] AuxButtons { private set; get; }
+
+        public override string ToString() => ToString(false);
+
+        public string ToString(bool isRaw)
+        {
+            if (isRaw)
+                return BitConverter.ToString(Raw);
+            else
+                return $"AuxButtons:[{String.Join(" ", AuxButtons)}]";
+        }
     }
 }
