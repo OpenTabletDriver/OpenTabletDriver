fcab958f174fffcc0fc300967a171140cf81719d
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2020-03-30 22:57:36 -0400

    Add stack trace to exception log message string

--- OpenTabletDriver/Windows/MainWindowViewModel.cs
+++ OpenTabletDriver/Windows/MainWindowViewModel.cs
@@ -728,8 +728,8 @@ namespace OpenTabletDriver.Windows
             var sb = new StringBuilder();
             foreach (var message in Messages)
             {
-                var line = string.Format("[{0}:{1}]\t{2}", message.IsError ? "Error" : "Normal", message.Group, message.Message);
-                sb.AppendLine(line);
+                var text = Log.GetStringFormat(message);
+                sb.AppendLine(text);
             }
             return sb.ToString();
         }
