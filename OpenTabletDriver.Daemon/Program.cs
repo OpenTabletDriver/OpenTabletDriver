ccb246607ac413f0971d52c8839aea8dedf269c4
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2020-06-12 23:13:26 -0400

    Add ability to run as a windows service

--- OpenTabletDriver.Daemon/Program.cs
+++ OpenTabletDriver.Daemon/Program.cs
@@ -11,6 +11,7 @@ using TabletDriverPlugin;
 using System.CommandLine;
 using System.IO;
 using System.CommandLine.Invocation;
+using NativeLib;
 
 namespace OpenTabletDriver.Daemon
 {
@@ -29,17 +30,26 @@ namespace OpenTabletDriver.Daemon
                 new Option(new string[] { "--config", "-c" }, "Configuration directory")
                 {
                     Argument = new Argument<DirectoryInfo> ("config")
+                },
+                new Option(new string[] { "--service", "-s"}, "Run as a service")
+                {
+                    Argument = new Argument<bool>("runAsService")
                 }
             };
-            rootCommand.Handler = CommandHandler.Create<DirectoryInfo, DirectoryInfo>((appdata, config) => 
+            rootCommand.Handler = CommandHandler.Create<DirectoryInfo, DirectoryInfo, bool>((appdata, config, runAsService) => 
             {
                 AppInfo.AppDataDirectory = appdata;
                 AppInfo.ConfigurationDirectory = config;
+                RunAsService = runAsService;
+                
             });
             rootCommand.Invoke(args);
 
             Daemon = new DriverDaemon();
-            await CreateHostBuilder().Build().RunAsync();
+            var hostBuilder = CreateHostBuilder();
+            if (RunAsService && PlatformInfo.IsWindows)
+                hostBuilder = hostBuilder.UseWindowsService();
+            await hostBuilder.Build().RunAsync();
         }
 
         static IHostBuilder CreateHostBuilder() => 
@@ -53,10 +63,11 @@ namespace OpenTabletDriver.Daemon
                     builder.AddNamedPipeEndpoint<IDriverDaemon>("OpenTabletDriver");
                 })
                 .ConfigureLogging(builder => 
-                {                    
+                {
                 });
 
         static DriverDaemon Daemon { set; get; }
         static bool Running { set; get; }
+        static bool RunAsService { set; get; }
     }
 }
