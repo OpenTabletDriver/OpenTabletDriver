8c828cd824e878ed81368292fc062bad4c84b616
krow <40317194+kroware@users.noreply.github.com>
2020-06-17 13:24:59 +0800

    add missing file

---
+++ NativeLib/OSX/Generic/CFUUIDBytes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NativeLib.OSX.Generic
+{
+    using UInt8 = Byte;
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct CFUUIDBytes
+    {
+        UInt8 byte0;
+        UInt8 byte1;
+        UInt8 byte2;
+        UInt8 byte3;
+        UInt8 byte4;
+        UInt8 byte5;
+        UInt8 byte6;
+        UInt8 byte7;
+        UInt8 byte8;
+        UInt8 byte9;
+        UInt8 byte10;
+        UInt8 byte11;
+        UInt8 byte12;
+        UInt8 byte13;
+        UInt8 byte14;
+        UInt8 byte15;
+	}
+}
