0fffb40378586e52f59fb16978a051a7817cd8c1
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2020-10-03 12:38:35 -0400

    Add Plugin tests

---
+++ OpenTabletDriver.Tests/PluginTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenTabletDriver.Plugin.Tablet;
+
+namespace OpenTabletDriver.Tests
+{
+    [TestClass]
+    public class PluginTests
+    {
+        [DataTestMethod]
+        [DataRow("[0..16)", 16, false)]
+        [DataRow("[0..16)", 0, true)]
+        [DataRow("(0..16]", 16, true)]
+        [DataRow("(0..16]", 0, false)]
+        public void RangeTest(string rangeStr, int testValue, bool expectedResult)
+        {
+            var range = DetectionRange.Parse(rangeStr);
+            var result = range.IsInRange(testValue);
+            Debug.Assert(result == expectedResult);
+        }
+    }
+}
