b89d1bb08b8d3a918478d129447183b26b76a4e3
X9VoiD <oscar.silvestrexx@gmail.com>
2020-10-21 05:18:44 +0800

    Reject double periods

--- OpenTabletDriver.UX/Tools/UXTools.cs
+++ OpenTabletDriver.UX/Tools/UXTools.cs
@@ -8,6 +8,12 @@ namespace OpenTabletDriver.UX.Tools
     {
         public static void RestrictToNumber(object _, TextChangingEventArgs args)
         {
+            if (Regex.IsMatch(args.NewText, ".*\\..*\\."))
+            {
+                args.Cancel = true;
+                return;
+            }
+
             var text = Regex.Replace(args.Text, "\\.$", ".0");
             args.Cancel = !double.TryParse(text, out var _);
         }
