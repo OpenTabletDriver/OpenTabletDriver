0ba918a736693de55cd3873deecc77d6fb1a15c9
AbstractQbit <38468635+AbstractQbit@users.noreply.github.com>
2020-10-24 16:47:29 +0300

    Fix rotation not being accounted for when ignoring outside area

--- OpenTabletDriver.Plugin/Output/AbsoluteOutputMode.cs
+++ OpenTabletDriver.Plugin/Output/AbsoluteOutputMode.cs
@@ -12,9 +12,9 @@ namespace OpenTabletDriver.Plugin.Output
     public abstract class AbsoluteOutputMode : BindingHandler, IOutputMode
     {
         private float _halfDisplayWidth, _halfDisplayHeight, _halfTabletWidth, _halfTabletHeight;
-        private Vector2 _minRawArea, _maxRawArea;
-        private Vector2 _minOut, _maxOut;
+        private Vector2 _min, _max;
         private Matrix3x2 _transformationMatrix;
+        private Matrix3x2 _areaCheckMatrix;
 
         private List<IFilter> _filters, _preFilters = new List<IFilter>(), _postFilters = new List<IFilter>();
         public IEnumerable<IFilter> Filters
@@ -75,7 +75,10 @@ namespace OpenTabletDriver.Plugin.Output
         internal void UpdateCache()
         {
             if (!(Input is null | Output is null | Digitizer is null))
+            {
                 _transformationMatrix = CalculateTransformation(Input, Output, Digitizer);
+                _areaCheckMatrix = CalculateAreaCheckMatrix(Input, Digitizer);
+            }
 
             _halfDisplayWidth = Output?.Width / 2 ?? 0;
             _halfDisplayHeight = Output?.Height / 2 ?? 0;
@@ -83,28 +86,14 @@ namespace OpenTabletDriver.Plugin.Output
             _halfTabletHeight = Input?.Height / 2 ?? 0;
 
             float _minX, _maxX, _minY, _maxY;
-            {
-                _minX = Input?.Position.X - _halfTabletWidth ?? 0;
-                _maxX = Input?.Position.X + Input?.Width - _halfTabletWidth ?? 0;
-                _minY = Input?.Position.Y - _halfTabletHeight ?? 0;
-                _maxY = Input?.Position.Y + Input?.Height - _halfTabletHeight ?? 0;
 
-                var _mmToRaw = Matrix3x2.CreateScale(
-                    Digitizer?.MaxX / Digitizer?.Width ?? 0,
-                    Digitizer?.MaxY / Digitizer?.Height ?? 0);
+            _minX = Output?.Position.X - _halfDisplayWidth ?? 0;
+            _maxX = Output?.Position.X + Output?.Width - _halfDisplayWidth ?? 0;
+            _minY = Output?.Position.Y - _halfDisplayHeight ?? 0;
+            _maxY = Output?.Position.Y + Output?.Height - _halfDisplayHeight ?? 0;
 
-                _minRawArea = Vector2.Transform(new Vector2(_minX, _minY), _mmToRaw);
-                _maxRawArea = Vector2.Transform(new Vector2(_maxX, _maxY), _mmToRaw);
-            }
-            {
-                _minX = Output?.Position.X - _halfDisplayWidth ?? 0;
-                _maxX = Output?.Position.X + Output?.Width - _halfDisplayWidth ?? 0;
-                _minY = Output?.Position.Y - _halfDisplayHeight ?? 0;
-                _maxY = Output?.Position.Y + Output?.Height - _halfDisplayHeight ?? 0;
-
-                _minOut = new Vector2(_minX, _minY);
-                _maxOut = new Vector2(_maxX, _maxY);
-            }
+            _min = new Vector2(_minX, _minY);
+            _max = new Vector2(_maxX, _maxY);
         }
 
         internal Matrix3x2 CalculateTransformation(Area input, Area output, DigitizerIdentifier tablet)
@@ -133,6 +122,28 @@ namespace OpenTabletDriver.Plugin.Output
             return res;
         }
 
+        internal Matrix3x2 CalculateAreaCheckMatrix(Area input, DigitizerIdentifier tablet)
+        {
+            // Convert raw tablet data to millimeters
+            var res = Matrix3x2.CreateScale(
+                tablet.Width / tablet.MaxX,
+                tablet.Height / tablet.MaxY);
+
+            // Translate to the center of input area
+            res *= Matrix3x2.CreateTranslation(
+                -input.Position.X, -input.Position.Y);
+
+            // Apply rotation
+            res *= Matrix3x2.CreateRotation(
+                (float)(-input.Rotation * System.Math.PI / 180));
+
+            // Scale millimeters to unit
+            res *= Matrix3x2.CreateScale(
+                1 / input.Width, 1 / input.Height);
+
+            return res;
+        }
+
         public virtual void Read(IDeviceReport report)
         {
             if (report is ITabletReport tabletReport)
@@ -141,7 +152,7 @@ namespace OpenTabletDriver.Plugin.Output
                 {
                     if (IgnoreOutsideArea && !IsInsideArea(tabletReport))
                         return;
-                    
+
                     if (VirtualTablet is IPressureHandler pressureHandler)
                         pressureHandler.SetPressure((float)tabletReport.Pressure / (float)Digitizer.MaxPressure);
 
@@ -164,7 +175,7 @@ namespace OpenTabletDriver.Plugin.Output
 
             // Clipping to display bounds
             if (AreaClipping)
-                pos = Vector2.Clamp(pos, _minOut, _maxOut);
+                pos = Vector2.Clamp(pos, _min, _max);
 
             // Post Filter
             foreach (IFilter filter in _postFilters)
@@ -176,9 +187,10 @@ namespace OpenTabletDriver.Plugin.Output
         internal bool IsInsideArea(ITabletReport report)
         {
             var pos = new Vector2(report.Position.X, report.Position.Y);
+            pos = Vector2.Transform(pos, _areaCheckMatrix);
 
-            if (pos.X < _minRawArea.X | pos.X > _maxRawArea.X |
-                pos.Y < _minRawArea.Y | pos.Y > _maxRawArea.Y)
+            if (pos.X < -0.5 | pos.X > 0.5 |
+                pos.Y < -0.5 | pos.Y > 0.5)
                 return false;
             else
                 return true;
