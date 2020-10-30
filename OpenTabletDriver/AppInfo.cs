ec9fa1a57ad94c6717a02ae99176051a2fd20edd
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2020-10-29 20:03:46 -0400

    Fix ProgramDirectory path

--- OpenTabletDriver/AppInfo.cs
+++ OpenTabletDriver/AppInfo.cs
@@ -14,38 +14,44 @@ namespace OpenTabletDriver
         public string ConfigurationDirectory
         {
             set => this.configDirectory = value;
-            get => this.configDirectory ?? this.defaultConfigurationDirectory.Value;
+            get => this.configDirectory ??= DefaultConfigurationDirectory;
         }
 
         public string AppDataDirectory
         {
             set => this.appDataDirectory = value;
-            get => this.appDataDirectory ?? this.defaultAppDataDirectory.Value;
+            get => this.appDataDirectory ??= DefaultAppDataDirectory;
         }
 
         public string SettingsFile => Path.Join(AppDataDirectory, "settings.json");
         public string PluginDirectory => Path.Join(AppDataDirectory, "Plugins");
 
-        private static string ProgramDirectory => Assembly.GetEntryAssembly().Location;
+        private static string ProgramDirectory => Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
 
-        private readonly Lazy<string> defaultConfigurationDirectory = new Lazy<string>(() => 
+        private static string DefaultConfigurationDirectory
         {
-            var path = Path.Join(Environment.CurrentDirectory, "Configurations");
-            var fallbackPath = Path.Join(ProgramDirectory, "Configurations");
-            return Directory.Exists(path) ? path : Directory.Exists(fallbackPath) ? fallbackPath : null;
-        });
-
-        private readonly Lazy<string> defaultAppDataDirectory = new Lazy<string>(() => 
-        {
-            var path = Path.Join(ProgramDirectory, "userdata");
-            var fallbackPath = SystemInfo.CurrentPlatform switch
+            get
             {
-                RuntimePlatform.Windows => Path.Join(Environment.GetEnvironmentVariable("LOCALAPPDATA"), "OpenTabletDriver"),
-                RuntimePlatform.Linux   => Path.Join(Environment.GetEnvironmentVariable("HOME"), ".config", "OpenTabletDriver"),
-                RuntimePlatform.MacOS   => Path.Join(Environment.GetEnvironmentVariable("HOME"), "Library", "Application Support", "OpenTabletDriver"),
-                _                       => null
-            };
-            return Directory.Exists(path) ? path : Directory.Exists(fallbackPath) ? fallbackPath : null;
-        });
+                var path = Path.Join(ProgramDirectory, "Configurations");
+                var fallbackPath = Path.Join(Environment.CurrentDirectory, "Configurations");
+                return Directory.Exists(path) ? path : fallbackPath;
+            }
+        }
+
+        private static string DefaultAppDataDirectory
+        {
+            get
+            {
+                var path = Path.Join(ProgramDirectory, "userdata");
+                var fallbackPath = SystemInfo.CurrentPlatform switch
+                {
+                    RuntimePlatform.Windows => Path.Join(Environment.GetEnvironmentVariable("LOCALAPPDATA"), "OpenTabletDriver"),
+                    RuntimePlatform.Linux   => Path.Join(Environment.GetEnvironmentVariable("HOME"), ".config", "OpenTabletDriver"),
+                    RuntimePlatform.MacOS   => Path.Join(Environment.GetEnvironmentVariable("HOME"), "Library", "Application Support", "OpenTabletDriver"),
+                    _                       => null
+                };
+                return Directory.Exists(path) ? path : fallbackPath;
+            }
+        }
     }
 }
