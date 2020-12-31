fc0a92f94ad45cf59d975229d1c9c1f9603c2505
X9VoiD <oscar.silvestrexx@gmail.com>
2020-12-31 15:35:12 +0800

    Terminate when running as admin

--- OpenTabletDriver.UX.Wpf/Program.cs
+++ OpenTabletDriver.UX.Wpf/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Principal;
+using Eto.Forms;
 
 namespace OpenTabletDriver.UX.Wpf
 {
@@ -7,6 +9,15 @@ namespace OpenTabletDriver.UX.Wpf
         [STAThread]
         public static void Main(string[] args)
         {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new(identity);
+            if (principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                _ = new Application(Eto.Platforms.Wpf);
+                MessageBox.Show("Do not run OpenTabletDriver as administrator.", MessageBoxType.Error);
+                Environment.Exit(1);
+            }
+
             App.Run(Eto.Platforms.Wpf, args);
         }
     }
