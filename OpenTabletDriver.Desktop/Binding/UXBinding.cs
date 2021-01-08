588787ac9c8f4877939b7e39ad278039a3515430
X9VoiD <oscar.silvestrexx@gmail.com>
2021-01-08 18:37:52 +0800

    Add UXBinding

---
+++ OpenTabletDriver.Desktop/Binding/UXBinding.cs
@@ -0,0 +1,38 @@
+using System;
+using OpenTabletDriver.Plugin;
+using OpenTabletDriver.Plugin.Attributes;
+
+namespace OpenTabletDriver.Desktop.Binding
+{
+    [PluginName("UX Binding")]
+    public class UXBinding : IBinding, IValidateBinding
+    {
+        public string[] ValidProperties => new string[]
+        {
+            "Show Window"
+        };
+
+        [Property("Property")]
+        public string Property { get; set; }
+
+        public Action Press => async () =>
+        {
+            switch (Property)
+            {
+                case "Show Window":
+                    try
+                    {
+                        var ux = await Info.GetUXInstance();
+                        await ux.ShowClient();
+                    }
+                    catch { }
+                    break;
+
+                default:
+                    break;
+            }
+        };
+
+        public Action Release => () => { };
+    }
+}
