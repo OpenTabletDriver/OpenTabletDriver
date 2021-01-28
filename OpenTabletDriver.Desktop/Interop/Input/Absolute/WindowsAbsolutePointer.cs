d96a2d7f15ec6018f3c566ef988d281b06c36170
X9VoiD <oscar.silvestrexx@gmail.com>
2021-01-28 21:22:26 +0800

    Cache and vectorize WindowsAbsolutePointer's conversion

--- OpenTabletDriver.Desktop/Interop/Input/Absolute/WindowsAbsolutePointer.cs
+++ OpenTabletDriver.Desktop/Interop/Input/Absolute/WindowsAbsolutePointer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Numerics;
+﻿using System.Numerics;
 using OpenTabletDriver.Native.Windows;
 using OpenTabletDriver.Native.Windows.Input;
 using OpenTabletDriver.Plugin.Platform.Pointer;
@@ -10,11 +9,14 @@ namespace OpenTabletDriver.Desktop.Interop.Input.Absolute
 
     public class WindowsAbsolutePointer : WindowsVirtualMouse, IAbsolutePointer
     {
+        private Vector2 ScreenToVirtualDesktop = new Vector2(SystemInterop.VirtualScreen.Width, SystemInterop.VirtualScreen.Height) / 65535;
+
         public void SetPosition(Vector2 pos)
         {
+            var virtualDesktopCoords = pos / ScreenToVirtualDesktop;
             inputs[0].U.mi.dwFlags = MOUSEEVENTF.ABSOLUTE | MOUSEEVENTF.MOVE | MOUSEEVENTF.VIRTUALDESK;
-            inputs[0].U.mi.dx = (int)(pos.X / SystemInterop.VirtualScreen.Width * 65535);
-            inputs[0].U.mi.dy = (int)(pos.Y / SystemInterop.VirtualScreen.Height * 65535);
+            inputs[0].U.mi.dx = (int)virtualDesktopCoords.X;
+            inputs[0].U.mi.dy = (int)virtualDesktopCoords.Y;
             SendInput(1, inputs, INPUT.Size);
         }
     }
