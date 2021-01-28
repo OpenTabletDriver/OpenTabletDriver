b53b32b49ef03dda9446d0b178410549dc9bd527
X9VoiD <oscar.silvestrexx@gmail.com>
2021-01-28 23:01:09 +0800

    Periodically refresh ScreenToVirtualDesktop

--- OpenTabletDriver.Desktop/Interop/Input/Absolute/WindowsAbsolutePointer.cs
+++ OpenTabletDriver.Desktop/Interop/Input/Absolute/WindowsAbsolutePointer.cs
@@ -9,11 +9,31 @@ namespace OpenTabletDriver.Desktop.Interop.Input.Absolute
 
     public class WindowsAbsolutePointer : WindowsVirtualMouse, IAbsolutePointer
     {
-        private Vector2 ScreenToVirtualDesktop = new Vector2(SystemInterop.VirtualScreen.Width, SystemInterop.VirtualScreen.Height) / 65535;
+        static WindowsAbsolutePointer()
+        {
+            timer = new System.Timers.Timer(250);
+            timer.Elapsed += (_, elapsedArgs) =>
+            {
+                lock (stateLock)
+                {
+                    ScreenToVirtualDesktop = new Vector2(SystemInterop.VirtualScreen.Width, SystemInterop.VirtualScreen.Height) / 65535;
+                }
+            };
+            timer.Start();
+        }
+
+        private static object stateLock;
+        private static System.Timers.Timer timer;
+        private static Vector2 ScreenToVirtualDesktop = new Vector2(SystemInterop.VirtualScreen.Width, SystemInterop.VirtualScreen.Height) / 65535;
 
         public void SetPosition(Vector2 pos)
         {
-            var virtualDesktopCoords = pos / ScreenToVirtualDesktop;
+            Vector2 virtualDesktopCoords;
+            lock (stateLock)
+            {
+                virtualDesktopCoords = pos / ScreenToVirtualDesktop;
+            }
+
             inputs[0].U.mi.dwFlags = MOUSEEVENTF.ABSOLUTE | MOUSEEVENTF.MOVE | MOUSEEVENTF.VIRTUALDESK;
             inputs[0].U.mi.dx = (int)virtualDesktopCoords.X;
             inputs[0].U.mi.dy = (int)virtualDesktopCoords.Y;
