08dced60e7e49bca0f3553ad1867a2974631048f
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2021-02-01 11:29:14 -0500

    Replace tabs with spaces

--- OpenTabletDriver.UX/Controls/Generic/Text/Providers/NumberTextProvider.cs
+++ OpenTabletDriver.UX/Controls/Generic/Text/Providers/NumberTextProvider.cs
@@ -20,13 +20,13 @@ namespace OpenTabletDriver.UX.Controls.Generic.Text.Providers
             {
                 builder.Clear();
                 if (value != null)
-				{
-					int pos = 0;
-					foreach (char ch in value)
-					{
-						Insert(ch, ref pos);
-					}
-				}
+                {
+                    int pos = 0;
+                    foreach (char ch in value)
+                    {
+                        Insert(ch, ref pos);
+                    }
+                }
             }
             get => builder.ToString();
         }
@@ -38,9 +38,9 @@ namespace OpenTabletDriver.UX.Controls.Generic.Text.Providers
             get
             {
                 for (int i = 0; i <= builder.Length; i++)
-				{
-					yield return i;
-				}
+                {
+                    yield return i;
+                }
             }
         }
 
@@ -54,18 +54,18 @@ namespace OpenTabletDriver.UX.Controls.Generic.Text.Providers
         public bool Delete(ref int position, int length, bool forward)
         {
             if (builder.Length == 0)
-				return false;
-			if (forward)
-			{
-				length = Math.Min(length, builder.Length - position);
-				builder.Remove(position, length);
-			}
-			else if (position >= length)
-			{
-				builder.Remove(position - length, length);
-				position = Math.Max(0, position - length);
-			}
-			return true;
+                return false;
+            if (forward)
+            {
+                length = Math.Min(length, builder.Length - position);
+                builder.Remove(position, length);
+            }
+            else if (position >= length)
+            {
+                builder.Remove(position - length, length);
+                position = Math.Max(0, position - length);
+            }
+            return true;
         }
 
         public bool Insert(char character, ref int position)
@@ -88,57 +88,57 @@ namespace OpenTabletDriver.UX.Controls.Generic.Text.Providers
                 builder[position] = character;
                 position++;
             }
-			return true;
+            return true;
         }
 
         private bool Allow(ref char character, ref int position)
-		{
-			bool allow = false;
-			if (!allow && character == DECIMAL_CHAR)
-			{
-				character = DECIMAL_CHAR;
-				var decimalIndex = Text.IndexOf(DECIMAL_CHAR);
+        {
+            bool allow = false;
+            if (!allow && character == DECIMAL_CHAR)
+            {
+                character = DECIMAL_CHAR;
+                var decimalIndex = Text.IndexOf(DECIMAL_CHAR);
 
-				if (decimalIndex >= 0)
-				{
-					builder.Remove(decimalIndex, 1);
-					if (position > decimalIndex)
-						position--;
-				}
+                if (decimalIndex >= 0)
+                {
+                    builder.Remove(decimalIndex, 1);
+                    if (position > decimalIndex)
+                        position--;
+                }
 
-				allow = true;
-				if (position < builder.Length && !char.IsDigit(builder[position]))
-				{
-					// insert at correct location and move cursor
-					int idx;
-					for (idx = 0; idx < builder.Length; idx++)
-					{
-						if (char.IsDigit(builder[idx]))
-						{
-							break;
-						}
-					}
-					position = idx;
-					allow = true;
-				}
-			}
+                allow = true;
+                if (position < builder.Length && !char.IsDigit(builder[position]))
+                {
+                    // insert at correct location and move cursor
+                    int idx;
+                    for (idx = 0; idx < builder.Length; idx++)
+                    {
+                        if (char.IsDigit(builder[idx]))
+                        {
+                            break;
+                        }
+                    }
+                    position = idx;
+                    allow = true;
+                }
+            }
 
-			if (!allow && character == '-')
-			{
-				var val = Text;
-				if (val.IndexOf('-') == 0)
-				{
-					builder.Remove(0, 1);
-					if (position == 0)
-						position++;
-				}
-				else
-					position++;
-				builder.Insert(0, character);
-				return false;
-			}
-			allow |= char.IsDigit(character);
-			return allow;
-		}
+            if (!allow && character == '-')
+            {
+                var val = Text;
+                if (val.IndexOf('-') == 0)
+                {
+                    builder.Remove(0, 1);
+                    if (position == 0)
+                        position++;
+                }
+                else
+                    position++;
+                builder.Insert(0, character);
+                return false;
+            }
+            allow |= char.IsDigit(character);
+            return allow;
+        }
     }
 }
