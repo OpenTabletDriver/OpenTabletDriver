81af75fd1f7f922d256cb62d5bc74c71e14710cc
X9VoiD <oscar.silvestrexx@gmail.com>
2021-03-06 02:12:11 +0800

    Add MacOSInteropBenchmark

---
+++ OpenTabletDriver.Benchmarks/Output/MacOSInteropBenchmark.cs
@@ -0,0 +1,25 @@
+using System.Numerics;
+using BenchmarkDotNet.Attributes;
+using OpenTabletDriver.Desktop.Interop.Input.Absolute;
+using OpenTabletDriver.Desktop.Interop.Input.Relative;
+
+namespace OpenTabletDriver.Benchmarks.Output
+{
+    public class MacOSInteropBenchmark
+    {
+        private MacOSAbsolutePointer absolutePointer = new MacOSAbsolutePointer();
+        private MacOSRelativePointer relativePointer = new MacOSRelativePointer();
+
+        [Benchmark]
+        public void CoreGraphicsAbsolute()
+        {
+            absolutePointer.SetPosition(Vector2.Zero);
+        }
+
+        [Benchmark]
+        public void CoreGraphicsRelative()
+        {
+            relativePointer.Translate(Vector2.Zero);
+        }
+    }
+}
