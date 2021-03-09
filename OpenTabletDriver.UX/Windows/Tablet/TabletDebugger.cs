aa4d40c8583a3b68fc842fff79866116b17a83b6
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-03-09 14:12:20 +0100

    added pressure visualization to tablet debugger

--- OpenTabletDriver.UX/Windows/Tablet/TabletDebugger.cs
+++ OpenTabletDriver.UX/Windows/Tablet/TabletDebugger.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel;
 using Eto.Drawing;
 using Eto.Forms;
@@ -189,7 +189,8 @@ namespace OpenTabletDriver.UX.Windows.Tablet
                     var tabletScale = tabletMm / tabletPx * scale;
                     var position = new PointF(tabletReport.Position.X, tabletReport.Position.Y) * tabletScale;
 
-                    var drawRect = RectangleF.FromCenter(position, new SizeF(5, 5));
+                    var circleSize = 5 + tabletReport.Pressure * 20 / tablet.Digitizer.MaxPressure;
+                    var drawRect = RectangleF.FromCenter(position, new SizeF(circleSize, circleSize));
                     graphics.FillEllipse(AccentColor, drawRect);
                 }
             }
