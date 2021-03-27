e91a375e941158cceee14f3b432983ccf00d0fd9
X9VoiD <oscar.silvestrexx@gmail.com>
2021-03-27 16:32:34 +0800

    Fix async element issues and provide PenIsInRange helper function

--- OpenTabletDriver.Plugin/Output/AsyncPositionedPipelineElement.cs
+++ OpenTabletDriver.Plugin/Output/AsyncPositionedPipelineElement.cs
@@ -2,38 +2,17 @@ using System;
 using OpenTabletDriver.Plugin.Attributes;
 using OpenTabletDriver.Plugin.DependencyInjection;
 using OpenTabletDriver.Plugin.Timers;
+using OpenTabletDriver.Plugin.Timing;
 
 namespace OpenTabletDriver.Plugin.Output
 {
     public abstract class AsyncPositionedPipelineElement<T> : IPositionedPipelineElement<T>, IDisposable
     {
+        private readonly object synchronizationObject = new object();
+        private HPETDeltaStopwatch consumeWatch = new HPETDeltaStopwatch();
         private ITimer scheduler;
+        private float? reportMsAvg;
         private float frequency;
-
-        [Resolved]
-        public ITimer Scheduler
-        {
-            set
-            {
-                this.scheduler = value;
-                this.scheduler.Elapsed += OnEmit;
-                this.scheduler.Start();
-            }
-            get => this.scheduler;
-        }
-
-        public abstract PipelinePosition Position { get; }
-
-        [PropertyAttribute("Frequency"), UnitAttribute("hz"), DefaultPropertyValue(1000.0f)]
-        public float Frequency
-        {
-            set
-            {
-                this.frequency = value;
-                this.scheduler.Interval = 1000.0f / value;
-            }
-            get => this.frequency;
-        }
 
         /// <summary>
         /// The current state of the <see cref="AsyncPositionedPipelineElement{T}"/>
@@ -42,22 +21,74 @@ namespace OpenTabletDriver.Plugin.Output
 
         public event Action<T> Emit;
 
+        public abstract PipelinePosition Position { get; }
+
+        [Resolved]
+        public ITimer Scheduler
+        {
+            set
+            {
+                this.scheduler = value;
+                this.scheduler.Elapsed += OnTimerElapsed;
+                this.scheduler.Start();
+            }
+            get => this.scheduler;
+        }
+
+        [PropertyAttribute("Frequency"), UnitAttribute("hz"), DefaultPropertyValue(1000.0f)]
+        public float Frequency
+        {
+            set
+            {
+                this.frequency = value;
+                if (Scheduler.Enabled)
+                    Scheduler.Stop();
+                Scheduler.Interval = 1000f / value;
+                Scheduler.Start();
+            }
+            get => this.frequency;
+        }
+
         /// <summary>
         /// Sets the internal state.
         /// </summary>
         /// <param name="value"></param>
         public virtual void Consume(T value)
         {
-            State = value;
+            lock (synchronizationObject)
+            {
+                State = value;
+                var consumeDelta = (float)consumeWatch.Restart().TotalMilliseconds;
+                if (consumeDelta < 150)
+                    reportMsAvg += ((consumeDelta - reportMsAvg) * 0.1f) ?? consumeDelta;
+            }
         }
 
         /// <summary>
-        /// Invoked to emit a report.
+        /// Updates the state of the <see cref="AsyncPositionedPipelineElement{T}"/> within a synchronized context.
         /// This is invoked by the <see cref="Scheduler"/> on the interval defined by <see cref="Frequency"/>.
+        /// The implementer must invoke <see cref="Emit"/> to continue the input processing.
         /// </summary>
-        protected virtual void OnEmit()
+        /// <remarks>
+        /// Call <see cref="PenIsInRange"/> to check if the pen is in range and avoid false emit.
+        /// </remarks>
+        protected abstract void UpdateState();
+
+        /// <summary>
+        /// Determines if pen is in tablet hover range
+        /// </summary>
+        /// <returns><see cref="true"/> if pen is in range</returns>
+        protected bool PenIsInRange()
         {
-            Emit?.Invoke(State);
+            return (float)consumeWatch.Elapsed.TotalMilliseconds < Math.Max(3, (reportMsAvg * 1.5f) ?? float.MaxValue);
+        }
+
+        private void OnTimerElapsed()
+        {
+            lock (synchronizationObject)
+            {
+                UpdateState();
+            }
         }
 
         public void Dispose()
