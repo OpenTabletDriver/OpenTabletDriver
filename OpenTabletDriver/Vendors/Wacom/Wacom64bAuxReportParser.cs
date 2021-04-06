ab5d56ca34cd0f333a874025df38cf6f9414f72c
AbstractQbit <38468635+AbstractQbit@users.noreply.github.com>
2021-04-06 15:04:42 +0300

    Clean up Wacom64bAuxReportParser

--- OpenTabletDriver/Vendors/Wacom/Wacom64bAuxReportParser.cs
+++ OpenTabletDriver/Vendors/Wacom/Wacom64bAuxReportParser.cs
@@ -7,23 +7,11 @@ namespace OpenTabletDriver.Vendors.Wacom
     {
         public virtual IDeviceReport Parse(byte[] data)
         {
-            switch (data[2])
+            return data[2] switch
             {
-                case 0x80:
-                    return new AuxReport
-                    {
-                        Raw = data,
-                        AuxButtons = new bool[]
-                        {
-                            (data[3] & (1 << 0)) != 0,
-                            (data[3] & (1 << 1)) != 0,
-                            (data[3] & (1 << 2)) != 0,
-                            (data[3] & (1 << 3)) != 0
-                        }
-                    };
-                default:
-                    return new WacomTouchReport(data);
-            }
+                0x80 => new AuxReport(data),
+                _ => new WacomTouchReport(data)
+            };
         }
     }
 }
