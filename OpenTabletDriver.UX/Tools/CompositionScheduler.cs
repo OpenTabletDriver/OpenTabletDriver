01c9614ecc971722a239e6a4c005bc43c41c19d2
X9VoiD <oscar.silvestrexx@gmail.com>
2021-04-26 00:15:10 +0800

    Implement CompositionScheduler

---
+++ OpenTabletDriver.UX/Tools/CompositionScheduler.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Threading.Tasks;
+using Eto.Forms;
+
+namespace OpenTabletDriver.UX.Tools
+{
+    /// <summary>
+    /// A UX composition scheduler that fires events at a maximum of <see cref="MAX_FRAMES_PER_SEC"/>
+    /// and stops firing when there are no event handlers registered
+    /// </summary>
+    public static class CompositionScheduler
+    {
+        private static async Task SchedulerFunc()
+        {
+            while (running)
+            {
+                await Application.Instance.InvokeAsync(OnCompose);
+                await Task.Delay(1000 / MAX_FRAMES_PER_SEC);
+            }
+        }
+
+        private const int MAX_FRAMES_PER_SEC = 500;
+
+        public static void Register(EventHandler handler)
+        {
+            Compose += handler;
+            Refresh();
+        }
+
+        public static void Unregister(EventHandler handler)
+        {
+            Compose -= handler;
+            Refresh();
+        }
+
+        private static void Refresh()
+        {
+            if (Compose == null || Compose.GetInvocationList().Length == 0)
+                Stop();
+            else
+                Start();
+        }
+
+        private static void Start()
+        {
+            if (!running)
+            {
+                running = true;
+                Task.Run(SchedulerFunc);
+            }
+        }
+
+        private static void Stop()
+        {
+            if (running)
+                running = false;
+        }
+
+        private static void OnCompose()
+        {
+            Compose?.Invoke(null, null);
+        }
+
+        private static bool running;
+        private static event EventHandler Compose;
+    }
+}
--- OpenTabletDriver.UX/Windows/Tablet/TabletDebugger.cs
+++ OpenTabletDriver.UX/Windows/Tablet/TabletDebugger.cs
@@ -141,7 +141,7 @@ namespace OpenTabletDriver.UX.Windows.Tablet
             protected override Color VerticalBackgroundColor => base.HorizontalBackgroundColor;
         }
 
-        private class TabletVisualizer : TimedDrawable
+        private class TabletVisualizer : ScheduledDrawable
         {
             private static readonly Color AccentColor = SystemColors.Highlight;
 
