3602c0b6e98a72685cda7f2757c0913fe465e42a
X9VoiD <oscar.silvestrexx@gmail.com>
2021-05-04 22:03:46 +0800

    Fix hex-based input boxes

--- OpenTabletDriver.UX/Controls/Generic/Text/HexNumberBox.cs
+++ OpenTabletDriver.UX/Controls/Generic/Text/HexNumberBox.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using System.Text.RegularExpressions;
 using Eto.Forms;
 using OpenTabletDriver.UX.Controls.Generic.Text.Providers;
 
@@ -11,12 +12,14 @@ namespace OpenTabletDriver.UX.Controls.Generic.Text
             Provider = new HexTextProvider();
         }
 
-        private class HexTextProvider : HexTextProvider<int>
+        private class HexTextProvider : RegexBasedTextProvider<int>
         {
+            protected override Regex Regex => new Regex(@"^(?:0x)?(?:[0-9A-F]{1,4})?$");
+
             public override int Value
             {
                 set => Text = "0x" + value.ToString("X4");
-                get => int.TryParse(Text, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out var result) ? result : default(int);
+                get => int.TryParse(Text.Replace("0x", string.Empty), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out var result) ? result : default;
             }
         }
     }
