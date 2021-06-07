14e8334f6a9647458467c810953ff3869268df84
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2021-06-06 21:24:07 -0400

    Merge udev utility project

---
+++ generate-rules.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+SRC_ROOT=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+[ ! -d "${SRC_ROOT}" ] && exit 100;
+
+PROJECT="${SRC_ROOT}/OpenTabletDriver.Tools.udev"
+FRAMEWORK="net5.0"
+
+TABLET_CONFIGURATIONS="${SRC_ROOT}/OpenTabletDriver/Configurations"
+RULES_FILE="${SRC_ROOT}/bin/99-opentabletdriver.rules"
+
+if [ "$#" -gt 0 ]; then
+  # Pass arguments to utility instead of using defaults
+  dotnet_args=($@)
+else
+  [ ! -d "${TABLET_CONFIGURATIONS}" ] && exit 101;
+  dotnet_args=("-v" "${TABLET_CONFIGURATIONS}" "${RULES_FILE}")
+fi
+
+dotnet run -p "${PROJECT}" -f "${FRAMEWORK}" -- ${dotnet_args[@]}
