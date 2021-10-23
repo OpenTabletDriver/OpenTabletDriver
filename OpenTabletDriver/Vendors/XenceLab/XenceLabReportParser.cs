a6836a170b1ba1a3635e1063faa719efe7afe23f
X9VoiD <oscar.silvestrexx@gmail.com>
2021-10-24 00:13:12 +0800

    Finalize XenceLabReportParser

--- OpenTabletDriver/Vendors/XenceLab/XenceLabReportParser.cs
+++ OpenTabletDriver/Vendors/XenceLab/XenceLabReportParser.cs
@@ -9,40 +9,13 @@ namespace OpenTabletDriver.Vendors.XenceLab
         public IDeviceReport Parse(byte[] report)
         {
             var reportByte = report[1];
-            if (reportByte.IsBitSet(5))
-            {
-                return new XP_PenTabletReport(report);
-            }
 
-            return (reportByte & 0xf0) switch
-            {
-                0xf0 => new XenceLabAuxReport(report),
-                _ => new DeviceReport(report)
-            };
+            if ((reportByte & 0xf0) == 0xf0)
+                return new XP_PenAuxReport(report);
+            else if (reportByte.IsBitSet(5))
+                return new XenceLabTabletReport(report);
+
+            return new DeviceReport(report);
         }
-    }
-
-    public struct XenceLabAuxReport : IAuxReport
-    {
-        public XenceLabAuxReport(byte[] report)
-        {
-            Raw = report;
-
-            var auxByte = report[2];
-            AuxButtons = new[]
-            {
-                auxByte.IsBitSet(0),
-                auxByte.IsBitSet(1),
-                auxByte.IsBitSet(2),
-                auxByte.IsBitSet(3),
-                auxByte.IsBitSet(4),
-                auxByte.IsBitSet(5),
-                auxByte.IsBitSet(6),
-                auxByte.IsBitSet(7)
-            };
-        }
-
-        public bool[] AuxButtons { get; set; }
-        public byte[] Raw { get; set; }
     }
 }
