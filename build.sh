23dfdaa09a283186585198f70fc348bff10af254
Noah M <10090332+Sublimelime@users.noreply.github.com>
2021-10-26 13:40:49 -0500

    Apply suggestions from gonX code review
    Shift added to allow providing extra arguments to dotnet publish
    
    Co-authored-by: Sebastian Jensen <gonx@gonx.dk>

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@
 # package.
 
 runtime=${1:-linux-x64}
+shift
 
 options=(--configuration='Release' --framework='net5' --self-contained='false' --output='./bin' /p:SuppressNETCoreSdkPreviewMessage=true /p:PublishTrimmed=false --runtime=$runtime)
 
@@ -12,12 +13,12 @@ echo "Building OpenTabletDriver with runtime $runtime."
 mkdir -p ./bin
 
 echo -e "\nBuilding Daemon...\n"
-dotnet publish OpenTabletDriver.Daemon ${options[@]}
+dotnet publish OpenTabletDriver.Daemon ${options[@]} $@
 
 echo -e "\nBuilding Console...\n"
-dotnet publish OpenTabletDriver.Console ${options[@]}
+dotnet publish OpenTabletDriver.Console ${options[@]} $@
 
 echo -e "\nBuilding GTK UX...\n"
-dotnet publish OpenTabletDriver.UX.Gtk ${options[@]}
+dotnet publish OpenTabletDriver.UX.Gtk ${options[@]} $@
 
 echo "Build finished. Binaries created in ./bin"
