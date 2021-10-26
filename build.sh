8cff2e2de9f75c748e9a8ab2d4b1c48b8460db88
Noah M <10090332+Sublimelime@users.noreply.github.com>
2021-10-26 13:03:04 -0500

    Add -p flag to silence dir-exists error
    Per Infinity's suggestion
    
    Co-authored-by: InfinityGhost <InfinityGhostGit@gmail.com>

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ runtime=${1:-linux-x64}
 options=(--configuration='Release' --framework='net5' --self-contained='false' --output='./bin' /p:SuppressNETCoreSdkPreviewMessage=true /p:PublishTrimmed=false --runtime=$runtime)
 
 echo "Building OpenTabletDriver with runtime $runtime."
-mkdir ./bin
+mkdir -p ./bin
 
 echo -e "\nBuilding Daemon...\n"
 dotnet publish OpenTabletDriver.Daemon ${options[@]}
