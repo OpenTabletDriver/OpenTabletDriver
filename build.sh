ac69827edfa6ebfb1920a37a2b5725bbfeadb9d2
Noah M <noahcodes@protonmail.com>
2021-10-26 11:42:24 -0500

    Extract options into variable

--- build.sh
+++ build.sh
@@ -4,38 +4,19 @@
 # Uses the same commands as those found in the PKGBUILD for the AUR
 # package.
 
+OPTIONS=--configuration Release --framework net5 --runtime linux-x64 --self-contained false --output "./bin" /p:SuppressNETCoreSdkPreviewMessage=true /p:PublishTrimmed=false
+
 echo "Building OpenTabletDriver."
 mkdir ./bin
 
 echo -e "\nBuilding Daemon...\n"
-dotnet publish OpenTabletDriver.Daemon     \
-  --configuration   Release                \
-  --framework       net5                   \
-  --runtime         linux-x64              \
-  --self-contained  false                  \
-  --output          "./bin"                \
-  /p:SuppressNETCoreSdkPreviewMessage=true \
-  /p:PublishTrimmed=false
+dotnet publish OpenTabletDriver.Daemon $(OPTIONS)
 
 echo -e "\nBuilding Console...\n"
-dotnet publish OpenTabletDriver.Console    \
-  --configuration   Release                \
-  --framework       net5                   \
-  --runtime         linux-x64              \
-  --self-contained  false                  \
-  --output          "./bin"                \
-  /p:SuppressNETCoreSdkPreviewMessage=true \
-  /p:PublishTrimmed=false
+dotnet publish OpenTabletDriver.Console $(OPTIONS)
 
 echo -e "\nBuilding GTK UX...\n"
-dotnet publish OpenTabletDriver.UX.Gtk     \
-  --configuration   Release                \
-  --framework       net5                   \
-  --runtime         linux-x64              \
-  --self-contained  false                  \
-  --output          "./bin"                \
-  /p:SuppressNETCoreSdkPreviewMessage=true \
-  /p:PublishTrimmed=false
+dotnet publish OpenTabletDriver.UX.Gtk $(OPTIONS)
 
 echo "Build finished. Binaries created in './bin'"
 
