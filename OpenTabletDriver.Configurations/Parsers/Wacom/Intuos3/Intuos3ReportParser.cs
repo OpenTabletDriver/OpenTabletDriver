e7ea8eec89901431cae01a4f2ab0f52a85ae9340
kuuube <hexagonisalie@gmail.com>
2021-12-24 10:41:44 -0500

    Fix intuos3 mouse hover and add iproximityreport

--- OpenTabletDriver.Configurations/Parsers/Wacom/Intuos3/Intuos3ReportParser.cs
+++ OpenTabletDriver.Configurations/Parsers/Wacom/Intuos3/Intuos3ReportParser.cs
@@ -23,6 +23,7 @@ namespace OpenTabletDriver.Configurations.Parsers.Wacom.Intuos3
             {
                 0xE0 => new IntuosV1TabletReport(data),
                 0xF0 => new Intuos3MouseReport(data),
+                0xB0 => new Intuos3MouseReport(data),
                 _ => new DeviceReport(data)
             };
         }
