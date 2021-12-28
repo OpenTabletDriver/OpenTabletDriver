67861c4050a25ae6f5b2a3fd6b46db5228dbee37
Mrcubix <tintamare91@gmail.com>
2021-12-28 20:31:50 +0100

    Added 10moon 1060 Configurations

---
+++ OpenTabletDriver.Configurations/Parsers/10moon/10moonReportParser.cs
@@ -0,0 +1,16 @@
+﻿using OpenTabletDriver.Plugin.Tablet;
+using OpenTabletDriver.Plugin;
+
+namespace OpenTabletDriver.Configurations.Parsers.TenMoon
+{
+    public class TenMoonReportParser : IReportParser<IDeviceReport>
+    {
+        public IDeviceReport Parse(byte[] report)
+        {
+            if (report[11] != 0xFF)
+                return new TenMoonAuxReport(report);
+            else
+                return new TenMoonTabletReport(report);
+        }
+    }
+}
