2a2bc4a1dd25e29b91bd9ac8c471cebf45f129ef
Aki <75532970+AkiSakurai@users.noreply.github.com>
2022-01-17 23:49:23 +0800

    On MacOS, prevent user from launching the app when it is translocated.

--- OpenTabletDriver.UX.MacOS/Program.cs
+++ OpenTabletDriver.UX.MacOS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Eto.Forms;
 
 namespace OpenTabletDriver.UX.MacOS
 {
@@ -7,6 +8,18 @@ namespace OpenTabletDriver.UX.MacOS
         [STAThread]
         public static void Main(string[] args)
         {
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (baseDirectory.StartsWith("/private/var/folders"))
+            {
+                _ = new Application(Eto.Platforms.Mac64);
+                MessageBox.Show(
+                    "OpenTabletDriver cannot be run from this folder. " +
+                    "Please move it to elsewhere, " +
+                    "such as to the Applications folder.");
+                return;
+            }
+
             App.Run(Eto.Platforms.Mac64, args);
         }
     }
