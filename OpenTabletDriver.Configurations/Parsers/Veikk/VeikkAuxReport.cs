c641fd260f680e7810f74ed5eea742f577eee095
jamesbt365 <jamesbt365@gmail.com>
2022-09-02 22:01:49 +0100

    Add up to 12 buttons on the VeikkAuxReport

--- OpenTabletDriver.Configurations/Parsers/Veikk/VeikkAuxReport.cs
+++ OpenTabletDriver.Configurations/Parsers/Veikk/VeikkAuxReport.cs
@@ -17,6 +17,10 @@ namespace OpenTabletDriver.Configurations.Parsers.Veikk
                 report[4].IsBitSet(5) && report[3].IsBitSet(0),
                 report[4].IsBitSet(6) && report[3].IsBitSet(0),
                 report[4].IsBitSet(7) && report[3].IsBitSet(0),
+                report[5].IsBitSet(0) && report[3].IsBitSet(0),
+                report[5].IsBitSet(1) && report[3].IsBitSet(0),
+                report[5].IsBitSet(2) && report[3].IsBitSet(0),
+                report[5].IsBitSet(3) && report[3].IsBitSet(0),
             };
         }
 
