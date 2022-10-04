ea4be61e3f7a5e8005dfd73f1c0f9005ad02f4df
Adryzz <adryzz@vignetteapp.org>
2022-10-04 18:03:18 +0200

    start porting regex to new source generator

--- OpenTabletDriver.Desktop/Migration/MigrationUtils.cs
+++ OpenTabletDriver.Desktop/Migration/MigrationUtils.cs
@@ -7,7 +7,7 @@ using Newtonsoft.Json;
 
 namespace OpenTabletDriver.Desktop.Migration
 {
-    internal static class MigrationUtils
+    internal static partial class MigrationUtils
     {
         public static TValue? SerializeMigrate<T, TValue>(this T obj) where T : IMigrate<TValue>
         {
@@ -43,10 +43,22 @@ namespace OpenTabletDriver.Desktop.Migration
 
         private static readonly Dictionary<Regex, string> NamespaceMigrationDict = new Dictionary<Regex, string>
         {
-            { new Regex(@"TabletDriverLib\.(.+?)$"), @"OpenTabletDriver.{0}" },
-            { new Regex(@"OpenTabletDriver\.Binding\.(.+?)$"), @"OpenTabletDriver.Desktop.Binding.{0}" },
-            { new Regex(@"OpenTabletDriver\.Output\.(.+?)$"), @"OpenTabletDriver.Desktop.Output.{0}" },
-            { new Regex(@"OpenTabletDriver\.Plugin\.(.+?)$"), @"OpenTabletDriver.{0}"}
+            { TabletDriverLibRegex(), @"OpenTabletDriver.{0}" },
+            { BindingRegex(), @"OpenTabletDriver.Desktop.Binding.{0}" },
+            { OutputRegex(), @"OpenTabletDriver.Desktop.Output.{0}" },
+            { PluginRegex(), @"OpenTabletDriver.{0}"}
         };
+
+        [GeneratedRegex(@"TabletDriverLib\.(.+?)$")]
+        private static partial Regex TabletDriverLibRegex();
+
+        [GeneratedRegex(@"OpenTabletDriver\.Binding\.(.+?)$")]
+        private static partial Regex BindingRegex();
+
+        [GeneratedRegex(@"OpenTabletDriver\.Output\.(.+?)$")]
+        private static partial Regex OutputRegex();
+
+        [GeneratedRegex(@"OpenTabletDriver\.Plugin\.(.+?)$")]
+        private static partial Regex PluginRegex();
     }
 }
