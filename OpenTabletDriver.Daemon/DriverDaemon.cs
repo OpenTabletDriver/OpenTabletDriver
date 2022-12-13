dc7fba0c1e7dc4460200b2f4187922dd7532b001
X9VoiD <oscar.silvestrexx@gmail.com>
2022-12-13 18:38:32 +0000

    Set process priority to high by default

--- OpenTabletDriver.Daemon/DriverDaemon.cs
+++ OpenTabletDriver.Daemon/DriverDaemon.cs
@@ -444,6 +444,8 @@ namespace OpenTabletDriver.Daemon
             switch (SystemInterop.CurrentPlatform)
             {
                 case SystemPlatform.Windows:
+                    System.Diagnostics.Process.GetCurrentProcess().PriorityClass = System.Diagnostics.ProcessPriorityClass.High;
+
                     var windows8 = new Version(6, 2, 9200, 0);
                     if (Environment.OSVersion.Version >= windows8)
                     {
