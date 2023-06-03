f99534e922a4cdbad059940ef00c6b00dc87de59
X9VoiD <oscar.silvestrexx@gmail.com>
2023-06-04 03:19:27 +0800

    Cleanup MacOS Program.cs

--- OpenTabletDriver.UX.MacOS/Program.cs
+++ OpenTabletDriver.UX.MacOS/Program.cs
@@ -7,30 +7,16 @@ namespace OpenTabletDriver.UX.MacOS
 {
     class Program
     {
-        private static bool checkAccessibility()
-        {
-            return Environment.OSVersion.Version < new Version(10, 4) ||
-                ApplicationServices.AXIsProcessTrusted();
-        }
-
-        private static bool checkInpuMonitoring()
-        {
-            return Environment.OSVersion.Version < new Version(10, 15) ||
-                IOKit.IOHIDCheckAccess(IOHIDRequestType.kIOHIDRequestTypeListenEvent) == IOHIDAccessType.kIOHIDAccessTypeGranted;
-        }
-
-        private static bool checkPermission()
-        {
-            return checkInpuMonitoring() && checkAccessibility();
-        }
-
         [STAThread]
         public static void Main(string[] args)
         {
-            if (!checkPermission())
-            {
-                Console.WriteLine($"{Environment.OSVersion.Version}");
+            var hasAccessibility = Environment.OSVersion.Version < new Version(10, 4) ||
+                ApplicationServices.AXIsProcessTrusted();
+            var hasInputMonitoring = Environment.OSVersion.Version < new Version(10, 15) ||
+                IOKit.IOHIDCheckAccess(IOHIDRequestType.kIOHIDRequestTypeListenEvent) == IOHIDAccessType.kIOHIDAccessTypeGranted;
 
+            if (!hasAccessibility || !hasInputMonitoring)
+            {
                 _ = new Application(Eto.Platforms.Mac64);
                 MessageBox.Show("OpenTabletDriver is likely missing OS permissions for Accessibility or Input Monitoring. " +
                     "To solve this, navigate to Settings -> System Preferences -> Security and Privacy -> Privacy, " +
