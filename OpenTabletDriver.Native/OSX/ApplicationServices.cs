7db64906a97293fea8d1305e49889f612bbb9741
Aki <75532970+AkiSakurai@users.noreply.github.com>
2023-06-03 12:31:28 +0800

    add permission checking for macos on hotfix branch

---
+++ OpenTabletDriver.Native/OSX/ApplicationServices.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OpenTabletDriver.Native.OSX.ApplicationServices
+{
+    public static class ApplicationServices
+    {
+        private const string libas = "/System/Library/Frameworks/ApplicationServices.framework/ApplicationServices";
+        [DllImport(libas, EntryPoint = "AXIsProcessTrusted")]
+        public static extern bool AXIsProcessTrusted();
+    }
+}
+
