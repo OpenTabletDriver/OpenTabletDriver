1ac11828b99f0326691661875c859418831f1008
Aki <75532970+AkiSakurai@users.noreply.github.com>
2023-06-08 21:46:27 +0800

    Request permission after permission checking on macos for hotfix branch

--- OpenTabletDriver.Native.MacOS/ApplicationServices.cs
+++ OpenTabletDriver.Native.MacOS/ApplicationServices.cs
@@ -1,11 +1,20 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace OpenTabletDriver.Native.MacOS.ApplicationServices
 {
     public static class ApplicationServices
     {
         private const string libas = "/System/Library/Frameworks/ApplicationServices.framework/ApplicationServices";
-        [DllImport(libas, EntryPoint = "AXIsProcessTrusted")]
+
+        private static IntPtr handle = LibSystem.dlopen(libas, 0);
+
+        public static IntPtr kAXTrustedCheckOptionPrompt = LibSystem.GetConstant(handle, "kAXTrustedCheckOptionPrompt");
+
+        [DllImport(libas)]
         public static extern bool AXIsProcessTrusted();
+
+        [DllImport(libas)]
+        public static extern bool AXIsProcessTrustedWithOptions(IntPtr options);
     }
 }
--- OpenTabletDriver.UX.MacOS/Program.cs
+++ OpenTabletDriver.UX.MacOS/Program.cs
@@ -1,29 +1,10 @@
 using System;
 using Eto.Forms;
-using OpenTabletDriver.Native.MacOS.ApplicationServices;
-using OpenTabletDriver.Native.MacOS.IOkit;
 
 namespace OpenTabletDriver.UX.MacOS
 {
     public class Program
     {
-        private static bool checkAccessibility()
-        {
-            return Environment.OSVersion.Version < new Version(10, 4) ||
-                ApplicationServices.AXIsProcessTrusted();
-        }
-
-        private static bool checkInpuMonitoring()
-        {
-            return Environment.OSVersion.Version < new Version(10, 15) ||
-                IOKit.IOHIDCheckAccess(IOHIDRequestType.kIOHIDRequestTypeListenEvent) == IOHIDAccessType.kIOHIDAccessTypeGranted;
-        }
-
-        private static bool checkPermission()
-        {
-            return checkInpuMonitoring() && checkAccessibility();
-        }
-
         [STAThread]
         public static void Main(string[] args)
         {
@@ -36,19 +17,10 @@ namespace OpenTabletDriver.UX.MacOS
                 return;
             }
 
-            if (!checkPermission())
+            if (PermissionHelper.HasPermissions())
             {
-                Console.WriteLine($"{Environment.OSVersion.Version}");
-
-                _ = new Application(Eto.Platforms.Mac64);
-                MessageBox.Show("OpenTabletDriver is likely missing OS permissions for Accessibility or Input Monitoring. " +
-                    "To solve this, navigate to Settings -> System Preferences -> Security and Privacy -> Privacy, " +
-                    "check both Accessibility and Input Monitoring. " +
-                    "If they are already checked, uncheck and recheck them.", MessageBoxType.Warning);
-                return;
+                new MacOSApp(args).Start();
             }
-
-            new MacOSApp(args).Start();
         }
     }
 }
