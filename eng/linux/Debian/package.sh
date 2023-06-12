e6abe351beef61d6e5b0f82fe19b1fa750355efe
X9VoiD <oscar.silvestrexx@gmail.com>
2023-06-13 04:16:28 +0800

    Allow skipping build and centralize desc to lib.sh

--- eng/linux/Debian/package.sh
+++ eng/linux/Debian/package.sh
@@ -4,13 +4,14 @@
 # provide a manpage for the binaries in /usr/bin, and add a source package
 # where the binaries could be built from.
 
+# increment this when releasing a new package of the same upstream version
+# where the only changes are to the packaging itself
 PKG_VER="1"
 PKG_FILE="${OTD_LNAME}-${OTD_VERSION}-${PKG_VER}-x64.deb"
 
 script_root="$(readlink -f $(dirname "${BASH_SOURCE[0]}"))"
 
 output="${1}"
-netRuntime="${2}"
 
 move_to_nested "${output}" "${output}/usr/lib/opentabletdriver"
 
@@ -18,7 +19,6 @@ echo "Copying generic files..."
 cp -R "${GENERIC_FILES}"/* "${output}/"
 
 echo "Debianizing..."
-
 mkdir -p "${output}/debian"
 touch "${output}/debian/control"
 
@@ -46,30 +46,21 @@ Section: misc
 Priority: optional
 Architecture: amd64
 Installed-Size: $(du -s "${output}" | cut -f1)
-Pre-Depends: udev, bash
+Pre-Depends: udev, bash (>= 4.4)
 Depends: libevdev2, libgtk-3-0, dotnet-runtime-6.0, ${shlibdeps}
 Recommends: libx11-6, libxrandr2
 Conflicts: ${OTD_NAME}
 Replaces: ${OTD_NAME}
 Maintainer: ${OTD_MAINTAINERS[1]}
 Description: A ${OTD_DESC}
- ${OTD_NAME} has the highest number of supported tablets with great
- compatibility across multiple platforms, packaged in an easy-to-use graphical
- user interface.
+ $(echo "${OTD_LONG_DESC}" | sed '2,$s/^/ /')
  .
- ${OTD_NAME} has support for multiple tablets from the following (non-exhaustive) OEMs:
-  * Wacom
-  * Huion
-  * XP-Pen
-  * XenceLabs
-  * Gaomon
-  * Veikk
+ $(echo "${OTD_LONG_DESC2}" | sed '2,$s/^/ /')
 Homepage: ${OTD_UPSTREAM_URL}
 Vcs-Browser: ${OTD_REPO_URL}
 Vcs-Git: ${OTD_GIT}
 EOF
 
 echo "Creating '${PKG_FILE}'..."
-
 move_to_nested "${output}" "${output}/opentabletdriver"
 dpkg-deb --build --root-owner-group "${output}/opentabletdriver" "${output}/${PKG_FILE}"
