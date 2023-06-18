8f70b8399f4cc697d0863cfd664579f3785d826e
X9VoiD <oscar.silvestrexx@gmail.com>
2023-06-18 19:35:05 +0800

    Extract non-platform-specific bash code

--- eng/linux/lib.sh
+++ eng/linux/lib.sh
@@ -1,73 +1,13 @@
 #!/usr/bin/env bash
 
-set -eu
-
-### Input Environment Variables
-
-VERSION_SUFFIX=${VERSION_SUFFIX:-}
+. "$(dirname "${BASH_SOURCE[0]}")/../lib.sh"
 
 ### Global variables
 
 PKG_SCRIPT_ROOT="$(readlink -f $(dirname "${BASH_SOURCE[0]}"))"
-REPO_ROOT="$(readlink -f "${PKG_SCRIPT_ROOT}/../../")"
 GENERIC_FILES="$(readlink -f "${PKG_SCRIPT_ROOT}/Generic")"
 
-### Global Descriptors
-
-OTD_NAME="OpenTabletDriver"
-OTD_LNAME="opentabletdriver"
-
-OTD_DESC="cross-platform open-source tablet driver"
-
-OTD_LONG_DESC="${OTD_NAME} has the highest number of supported tablets with great
-compatibility across multiple platforms, packaged in an easy-to-use graphical
-user interface."
-
-OTD_LONG_DESC2="${OTD_NAME} has support for multiple tablets from the following (non-exhaustive) OEMs:
- * Wacom
- * Huion
- * XP-Pen
- * XenceLabs
- * Gaomon
- * Veikk"
-
-OTD_MAINTAINERS=(
-  "InfinityGhost <infinityghostgit@gmail.com>" \
-  "X9VoiD <oscar.silvestrexx@gmail.com>" \
-)
-OTD_UPSTREAM_URL="https://opentabletdriver.net"
-OTD_REPO_URL="https://github.com/OpenTabletDriver/OpenTabletDriver"
-OTD_GIT="https://github.com/OpenTabletDriver/OpenTabletDriver.git"
-OTD_VERSION_BASE="$(sed -n 's|.*<VersionBase>\(.*\)</VersionBase>.*|\1|p' "${REPO_ROOT}/Directory.Build.props")"
-OTD_VERSION="${OTD_VERSION_BASE}${VERSION_SUFFIX}"
-
-OTD_DAEMON="otd-daemon"
-OTD_GUI="otd-gui"
-OTD_CLI="otd"
-
 ### Helper functions
-
-create_binary_tarball() {
-  local source="${1}"
-  local output="${2}"
-
-  local last_pwd="${PWD}"
-
-  output="$(readlink -f "${output}")"
-  cd "${source}/.."
-  tar -czf "${output}" "$(basename ${source})"
-
-  cd "${last_pwd}"
-}
-
-move_to_nested() {
-  local source="${1}"
-  local nested="${2}"
-
-  local contents="$(echo "${source}"/*)"
-  mkdir -p "${nested}"
-  mv ${contents} "${nested}"
-}
 
 generate_rules() {
   local output_file="${1}"
@@ -92,77 +32,4 @@ Type=Application
 Categories=Settings;
 StartupNotify=true
 EOF
-}
-
-copy_pixmap_assets() {
-  local output_folder="${1}"
-
-  echo "Copying pixmap assets to '${output_folder}'..."
-  mkdir -p "${output_folder}"
-  cp "${REPO_ROOT}/OpenTabletDriver.UX/Assets"/* "${output_folder}"
-}
-
-create_source_tarball() {
-  local output="${1}"
-  output="$(readlink -f "${output}")"
-
-  local tmp_dir="$(mktemp -d)"
-  local last_pwd="${PWD}"
-
-  local output_file_name="$(basename "${output}")"
-  output_file_name="${output_file_name%.tar.gz}"
-  local source_tmp_dir="${tmp_dir}/${output_file_name}"
-
-  echo "Creating source tarball..."
-
-  mkdir -p "${source_tmp_dir}"
-  cd "${tmp_dir}"
-
-  local source_files=(
-    "docs"
-    "eng"
-    "OpenTabletDriver"
-    "OpenTabletDriver.Benchmarks"
-    "OpenTabletDriver.Configurations"
-    "OpenTabletDriver.Console"
-    "OpenTabletDriver.Daemon"
-    "OpenTabletDriver.Desktop"
-    "OpenTabletDriver.Native"
-    "OpenTabletDriver.Plugin"
-    "OpenTabletDriver.Tests"
-    "OpenTabletDriver.Tools.udev"
-    "OpenTabletDriver.UX"
-    "OpenTabletDriver.UX.Gtk"
-    "OpenTabletDriver.UX.MacOS"
-    "OpenTabletDriver.UX.Wpf"
-    ".editorconfig"
-    "build.ps1"
-    "build.sh"
-    "CONTRIBUTING.md"
-    "Directory.Build.props"
-    "generate-rules.sh"
-    "LICENSE"
-    "nuget.config"
-    "OpenTabletDriver.Linux.slnf"
-    "OpenTabletDriver.MacOS.slnf"
-    "OpenTabletDriver.sln"
-    "OpenTabletDriver.Windows.slnf"
-    "README.md"
-    "TABLETS.md"
-  )
-
-  for file in "${source_files[@]}"; do
-    cp -r "${REPO_ROOT}/${file}" "${source_tmp_dir}"
-  done
-
-  find "${source_tmp_dir}" -type d \( -name "bin" -o -name "obj" \) -exec rm -rf {} +
-
-  if [ -f "${output}" ]; then
-    rm "${output}"
-  fi
-
-  tar -czf "${output}" "${output_file_name}"
-
-  cd "${last_pwd}"
-  rm -rf "${tmp_dir}"
 }
--- eng/linux/package.sh
+++ eng/linux/package.sh
@@ -1,116 +1,49 @@
 #!/usr/bin/env bash
 
 set -eu
+. "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
 
-output="dist"
-config="Release"
-framework="net6.0"
-netRuntime="linux-x64"
-isRelease="true"
-build="true"
-packageGen=""
-isPortable="false"
-singleFile="true"
-selfContained="false"
+NET_RUNTIME="linux-x64"
+
+PACKAGE_GEN=""
+PROJECTS=(
+  "OpenTabletDriver.Daemon"
+  "OpenTabletDriver.Console"
+  "OpenTabletDriver.UX.Gtk"
+)
 
 print_help() {
   echo "Usage: ${BASH_SOURCE[0]} [OPTIONS]..."
-  echo "Options:"
-  echo "  -o, --output <path>           Output directory for build artifacts (default: ./dist/)"
-  echo "  -c, --configuration <config>  Build configuration (default: Release)"
-  echo "  -f, --framework <framework>   Target framework (default: net6.0)"
-  echo "  -r, --runtime <runtime>       Target runtime (default: linux-x64)"
-  echo "  --release <bool>              Whether to output production binaries (default: true)"
-  echo "  --package <package>           Package generation script to run after build"
-  echo "  --build <bool>                Whether to build binaries (default: true)"
-  echo "  --portable <bool>             Whether to build portable binaries (default: false)"
-  echo "  --single-file <bool>          Whether to build single-file binaries (default: true)"
-  echo "  --self-contained <bool>       Whether to build self-contained binaries, implies --single-file (default: false)"
-  echo "  -h, --help                    Print this help message"
+  print_common_arg_help
+  echo
+  echo "Platform-specific options:"
+  echo "  --package <package_type>      Package generation script to run after build"
+  echo "                                (see eng/linux/* for available package types)"
   echo
   echo "Remarks:"
   echo "  Anything after '--', if it is specified, will be passed to dotnet publish as-is."
 }
 
-is_extra_args=false
-extra_options=()
+args=("$@")          # copy args
+remaining_args=()    # remaining args after parsing
+is_extra_args=false  # whether we've reached '--'
+extra_args=()        # args after '--'
 
-while [ $# -gt 0 ]; do
+parse_build_args "args" "remaining_args"
+
+while [ ${#remaining_args[@]} -gt 0 ]; do
   if $is_extra_args; then
-    extra_options=("$@")
+    extra_args=("$@")
     break
   fi
 
   case "$1" in
-    -o=*|--output=*)
-      output="${1#*=}"
-      ;;
-    -o|--output)
-      output="$2"
-      shift
-      ;;
-    -c=*|--configuration=*)
-      config="${1#*=}"
-      ;;
-    -c|--configuration)
-      config="$2"
-      shift
-      ;;
-    -f=*|--framework=*)
-      framework="${1#*=}"
-      ;;
-    -f|--framework)
-      framework="$2"
-      shift
-      ;;
-    -r=*|--runtime=*)
-      netRuntime="${1#*=}"
-      ;;
-    -r|--runtime)
-      netRuntime="$2"
-      shift
-      ;;
-    --release=*)
-      isRelease="${1#*=}"
-      ;;
-    --release)
-      isRelease="$2"
-      shift
-      ;;
-    --build=*)
-      build="${1#*=}"
-      ;;
-    --build)
-      build="$2"
-      shift
-      ;;
     --package=*)
-      packageGen="${1#*=}"
+      PACKAGE_GEN="${1#*=}"
       ;;
     --package)
-      packageGen="$2"
-      shift
-      ;;
-    --portable=*)
-      isPortable="${1#*=}"
-      ;;
-    --portable)
-      isPortable="$2"
-      shift
-      ;;
-    --single-file=*)
-      singleFile="${1#*=}"
-      ;;
-    --single-file)
-      singleFile="$2"
-      shift
-      ;;
-    --self-contained=*)
-      selfContained="${1#*=}"
-      ;;
-    --self-contained)
-      selfContained="$2"
-      shift
+      PACKAGE_GEN="$2"
+      shift_arr "remaining_args"
       ;;
     --)
       is_extra_args=true
@@ -121,98 +54,17 @@ while [ $# -gt 0 ]; do
       exit 1
       ;;
   esac
-  shift
+  shift_arr "remaining_args"
 done
 
-prev_path=${PWD}
-
-projects=(
-  "OpenTabletDriver.Daemon"
-  "OpenTabletDriver.Console"
-  "OpenTabletDriver.UX.Gtk"
-)
-
-options=(
-  --configuration "${config}"
-  --runtime "${netRuntime}"
-  --framework "${framework}"
-  --self-contained "${selfContained}"
-  --output "${output}"
-  /p:PublishTrimmed=false
-  /p:DebugType=embedded
-  /p:SuppressNETCoreSdkPreviewMessage=true
-)
-
-if [ "${isRelease}" != "true" ]; then
-  options+=( /p:SourceRevisionId=$(git rev-parse --short HEAD) )
-fi
-if [ "${selfContained}" == "true" ] || [ "${singleFile}" == "true" ]; then
-  options+=( /p:PublishSingleFile=true )
-fi
-
-handle_error() {
-  echo "Build failed!"
-  cd "${prev_path}"
-  exit 1
-}
-
-exit_with_error() {
-  echo "$1"
-  handle_error
-}
-
-trap handle_error ERR
-
-# Import helper functions
-. "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
-
-# Change dir to repo root
 cd "${REPO_ROOT}"
 
-# Sanity checks
-if [ ! -d "OpenTabletDriver" ]; then
-  exit_with_error "Could not find OpenTabletDriver folder!"
-fi
+prepare_build
+build "PROJECTS" "extra_args"
 
-if [ -n "${packageGen}" ] && [ "${isPortable}" = "true" ]; then
-  exit_with_error "Cannot build portable binaries and generate packages at the same time!"
-fi
-
-if [ -d "${output}" ]; then
-  echo "Cleaning old build outputs..."
-  for dir in "${output}"/*; do
-    dir_name=$(basename "${dir}")
-    if [ "${dir_name}" != "userdata" ]; then
-      if ! rm -rf "${dir}" 2>/dev/null; then
-        exit_with_error "Could not clean old build dirs. Please manually remove contents of ${output} folder."
-      fi
-    fi
-  done
-fi
-
-mkdir -p "${output}"
-
-if [ "${build}" = "true" ]; then
-  echo "Restoring packages..."
-  dotnet restore --verbosity quiet > /dev/null
-
-  echo "Running dotnet clean..."
-  dotnet clean --configuration "${config}" --verbosity quiet > /dev/null
-
-  if [ "${isPortable}" = "true" ]; then
-    mkdir -p "${output}/userdata"
-  fi
-
-  for project in "${projects[@]}"; do
-    echo -e "\nBuilding ${project}...\n"
-    dotnet publish "${project}" "${options[@]}"
-  done
-
-  echo -e "\nBuild finished! Binaries created in ${output}\n"
-fi
-
-if [ -n "${packageGen}" ]; then
-  package_script="${PKG_SCRIPT_ROOT}/${packageGen}/package.sh"
+if [ -n "${PACKAGE_GEN}" ]; then
+  echo -e "\nCreating package with type '${PACKAGE_GEN}'..."
+  package_script="${PKG_SCRIPT_ROOT}/${PACKAGE_GEN}/package.sh"
   if [ ! -f "${package_script}" ]; then
     exit_with_error "Could not find package generation script: ${package_script}"
   fi
@@ -221,9 +73,7 @@ if [ -n "${packageGen}" ]; then
 
   # child package.sh should expect to be run from the repo root and should write
   # the filename of the package generated to PKG_FILE
-  . "${package_script}" "${output}"
+  . "${package_script}" "${OUTPUT}"
 
-  echo -e "\nPackaging finished! Package created at '${output}/${PKG_FILE}'"
+  echo -e "\nPackaging finished! Package created at '${OUTPUT}/${PKG_FILE}'"
 fi
-
-cd "${prev_path}"
