0bc6d349c4499fde93d9886f9ff5f4ef8916054d
X9VoiD <oscar.silvestrexx@gmail.com>
2023-07-26 02:11:08 +0800

    MacOS: Send all pointer events on Sync()

--- OpenTabletDriver.Desktop/Interop/Input/Relative/MacOSRelativePointer.cs
+++ OpenTabletDriver.Desktop/Interop/Input/Relative/MacOSRelativePointer.cs
@@ -1,7 +1,8 @@
 using System;
+using System.Linq;
 using System.Numerics;
 using OpenTabletDriver.Native.MacOS;
-using OpenTabletDriver.Native.MacOS.Generic;
+using OpenTabletDriver.Native.MacOS.Input;
 using OpenTabletDriver.Platform.Display;
 using OpenTabletDriver.Platform.Pointer;
 
@@ -11,18 +12,38 @@ namespace OpenTabletDriver.Desktop.Interop.Input.Relative
 
     public class MacOSRelativePointer : MacOSVirtualMouse, IRelativePointer
     {
-        public MacOSRelativePointer(IVirtualScreen virtualScreen) : base(virtualScreen)
+        private CGPoint offset;
+
+        public MacOSRelativePointer(IVirtualScreen virtualScreen)
         {
+            var primary = virtualScreen.Displays.First();
+            offset = new CGPoint(primary.Position.X, primary.Position.Y);
         }
 
         public void SetPosition(Vector2 delta)
         {
-            var lastPos = GetPosition();
-            var newPos = lastPos + delta;
-            var cgPt = new CGPoint(newPos.X, newPos.Y) - Offset;
-            var mouseEventRef = CGEventCreateMouseEvent(IntPtr.Zero, MoveEvent, cgPt, PressedButtons);
-            CGEventPost(CGEventTapLocation.kCGHIDEventTap, mouseEventRef);
-            CFRelease(mouseEventRef);
+            QueuePendingPosition(delta.X, delta.Y);
+        }
+
+        protected override void SetPendingPosition(IntPtr mouseEvent, float x, float y)
+        {
+            CGEventSetLocation(mouseEvent, GetCursorPosition() + new CGPoint(x, y));
+            CGEventSetDoubleValueField(mouseEvent, CGEventField.mouseEventDeltaX, x);
+            CGEventSetDoubleValueField(mouseEvent, CGEventField.mouseEventDeltaY, y);
+        }
+
+        protected override void ResetPendingPosition(IntPtr mouseEvent)
+        {
+            CGEventSetDoubleValueField(mouseEvent, CGEventField.mouseEventDeltaX, 0);
+            CGEventSetDoubleValueField(mouseEvent, CGEventField.mouseEventDeltaY, 0);
+        }
+
+        private CGPoint GetCursorPosition()
+        {
+            var eventRef = CGEventCreate(IntPtr.Zero);
+            var pos = CGEventGetLocation(eventRef) + offset;
+            CFRelease(eventRef);
+            return pos;
         }
     }
 }
