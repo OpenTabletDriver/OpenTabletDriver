ef48f2c693a8d0f11d66c5b3a3ca032957135e04
hwsmm <hwsnemo@gmail.com>
2023-08-17 16:10:56 +0000

    BinaryTarball: Use Generic/package.sh

--- eng/linux/BinaryTarBall/package.sh
+++ eng/linux/BinaryTarBall/package.sh
@@ -4,28 +4,21 @@ PKG_FILE="${OTD_LNAME}-${OTD_VERSION}-x64.tar.gz"
 
 output="${1}"
 
-move_to_nested "${output}" "${output}/usr/local/lib/opentabletdriver"
+move_to_nested "${output}" "${output}/build"
 
-copy_generic_files "${output}"
-mv "${output}/usr/lib"/* "${output}/usr/local/lib"
-mkdir -p "${output}/etc/"
-mv "${output}/usr/local/lib/systemd" "${output}/etc"
+env PREFIX="/usr/local" "${GENERIC_FILES}/package.sh" "${output}"
+mkdir -p "${output}/usr/local/lib"
+mv "${output}/build" "${output}/usr/local/lib/opentabletdriver"
 
 echo "Patching wrapper scripts to point to '/usr/local/lib/opentabletdriver'..."
-mkdir -p "${output}/usr/local/bin"
-for exe in "${output}/usr/bin"/*; do
+for exe in "${output}/usr/local/bin"/*; do
   sed -i "s|/usr/lib|/usr/local/lib|" "${exe}"
-  mv "${exe}" "${output}/usr/local/bin/${exe##*/}"
 done
 
-echo "Removing unused directories..."
-rmdir "${output}/usr/bin"
-rmdir "${output}/usr/lib"
-
-generate_rules "${output}/etc/udev/rules.d/99-opentabletdriver.rules"
-generate_desktop_file "${output}/usr/local/share/applications/opentabletdriver.desktop"
+mkdir -p "${output}/etc/udev/rules.d/"
+mv "${output}/usr/local/lib/udev/rules.d/90-opentabletdriver.rules" "${output}/etc/udev/rules.d/90-opentabletdriver.rules"
+rm -r "${output}/usr/local/lib/udev/"
 sed -i "s|/usr/share|/usr/local/share|" "${output}/usr/local/share/applications/opentabletdriver.desktop"
-copy_pixmap_assets "${output}/usr/local/share/pixmaps"
 
 echo "Creating binary tarball '${output}/${PKG_FILE}'..."
 
