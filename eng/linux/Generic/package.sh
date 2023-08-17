2a37605ac4acdfc698b3e5ddd5ccf65ea78ab32d
X9VoiD <oscar.silvestrexx@gmail.com>
2023-08-17 20:23:57 +0000

    Fix generic packaging script
    - Removed additional sourcing
    - Added back the PKG_FILE to maintain consistency with other packaging scripts
    - Added support for `--build false`
    - Patch files when PREFIX is not `usr`

--- eng/linux/Generic/package.sh
+++ eng/linux/Generic/package.sh
@@ -1,18 +1,39 @@
 #!/usr/bin/env bash
 
-. "$(dirname "${BASH_SOURCE[0]}")/../lib.sh"
-
 # This is a generic packaging script intended to be used by package maintainers
 
+PKG_FILE="files"
 output="$(readlink -f "${1}")"
-PREFIX="${PREFIX:-/usr/local/}"
+PREFIX="${PREFIX:-usr}"
 
-mkdir -p "${output}/${PREFIX}"
+if ["${BUILD}" == "true"]; then
+  move_to_nested "${output}" "${output}/${PREFIX}/lib/opentabletdriver"
+else
+  mkdir -p "${output}/${PREFIX}"
+fi
+
 copy_generic_files "${output}/${PREFIX}"
 mkdir -p "${output}/${PREFIX}/share/doc/opentabletdriver"
 cp -v "${REPO_ROOT}/LICENSE" "${output}/${PREFIX}/share/doc/opentabletdriver/LICENSE"
 
-generate_rules "${output}/${PREFIX}/lib/udev/rules.d/90-opentabletdriver.rules"
-generate_desktop_file "${output}/${PREFIX}/share/applications/opentabletdriver.desktop"
 copy_pixmap_assets "${output}/${PREFIX}/share/pixmaps"
 copy_manpage "${output}/${PREFIX}/share/man"
+
+if [ "${PREFIX}" == "usr" ]; then
+  generate_rules "${output}/usr/lib/udev/rules.d/90-opentabletdriver.rules"
+else
+  generate_rules "${output}/etc/udev/rules.d/90-opentabletdriver.rules"
+fi
+
+generate_desktop_file "${output}/${PREFIX}/share/applications/opentabletdriver.desktop"
+
+if [ "${PREFIX}" != "usr" ]; then
+  echo "Patching wrapper scripts to point to '/${PREFIX}/bin/opentabletdriver'..."
+  for exe in "${output}/${PREFIX}/bin"/*; do
+    sed -i "s|/usr/lib|/${PREFIX}/lib|" "${exe}"
+  done
+
+  sed -i "s|/usr/share|/${PREFIX}/share|" "${output}/${PREFIX}/share/applications/opentabletdriver.desktop"
+fi
+
+move_to_nested "${output}" "${output}/files"
