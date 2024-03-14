8d1d6255f267d52aa109d16ae8993667e45e0232
jamesbt365 <jamesbt365@gmail.com>
2024-03-14 17:17:28 +0000

    Lint, apply automatic simplifications & document identifier attributes

--- OpenTabletDriver/Tablet/TabletConfiguration.cs
+++ OpenTabletDriver/Tablet/TabletConfiguration.cs
@@ -29,19 +29,19 @@ namespace OpenTabletDriver.Tablet
         [Required(ErrorMessage = $"Tablet {nameof(DigitizerIdentifiers)} are required")]
         [DisplayName("Digitizer Identifiers")]
         [MinLength(1, ErrorMessage = "Requires at least 1 identifier")]
-        public List<DeviceIdentifier> DigitizerIdentifiers { set; get; } = new List<DeviceIdentifier>();
+        public List<DeviceIdentifier> DigitizerIdentifiers { set; get; } = [];
 
         /// <summary>
         /// The auxiliary device identifier.
         /// </summary>
         [Required(ErrorMessage = $"Tablet {nameof(AuxiliaryDeviceIdentifiers)} must be present")]
         [DisplayName("Auxiliary Identifiers")]
-        public List<DeviceIdentifier> AuxiliaryDeviceIdentifiers { set; get; } = new List<DeviceIdentifier>();
+        public List<DeviceIdentifier> AuxiliaryDeviceIdentifiers { set; get; } = [];
 
         /// <summary>
         /// Other information about the tablet that can be used in tools or other applications.
         /// </summary>
         [Required(ErrorMessage = $"Tablet {nameof(Attributes)} must be present")]
-        public Dictionary<string, string> Attributes { set; get; } = new Dictionary<string, string>();
+        public Dictionary<string, string> Attributes { set; get; } = [];
     }
 }
