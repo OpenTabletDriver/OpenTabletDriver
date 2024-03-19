06d1cd5892decc37c0c635e1c6290ac56dce641e
chenyanming <elecming@gmail.com>
2024-03-19 10:53:57 +0800

    Add artist pro 16 gen2 support

--- OpenTabletDriver.Configurations/Parsers/XP_Pen/XP_PenTabletGen2Report.cs
+++ OpenTabletDriver.Configurations/Parsers/XP_Pen/XP_PenTabletGen2Report.cs
@@ -10,10 +10,19 @@ namespace OpenTabletDriver.Configurations.Parsers.XP_Pen
         {
             Raw = report;
 
+            const float tiltCorrectionFactor = 4.0f; // Experiment with this value for accurate alignment
+            Tilt = new Vector2
+            {
+                X = (sbyte)report[8],
+                Y = (sbyte)report[9]
+            };
+
             Position = new Vector2
             {
-                X = Unsafe.ReadUnaligned<ushort>(ref report[2]) | report[10] << 16,
+                X = (Unsafe.ReadUnaligned<ushort>(ref report[2]) | report[10] << 16)
+                - (int)(tiltCorrectionFactor * Tilt.X), // Adjust X position based on tilt angle
                 Y = Unsafe.ReadUnaligned<ushort>(ref report[4]) | report[11] << 16
+                - (int)(tiltCorrectionFactor * Tilt.Y) // Adjust Y position based on tilt angle
             };
             Pressure = (uint)((Unsafe.ReadUnaligned<ushort>(ref report[6]) - 16384) | (report[13] & 0x01) << 13);
             Eraser = report[1].IsBitSet(3);
@@ -22,12 +31,6 @@ namespace OpenTabletDriver.Configurations.Parsers.XP_Pen
             {
                 report[1].IsBitSet(1),
                 report[1].IsBitSet(2)
-            };
-
-            Tilt = new Vector2
-            {
-                X = (sbyte)report[8],
-                Y = (sbyte)report[9]
             };
         }
 
