9cfa4dfe335c0e1b6e19c6ea56f267672c69d6d9
Spencer Sawyer <S@spswy.com>
2024-03-19 20:06:03 -0700

    Added second out of range state that I was experiencing from my 1060. Valid Report[1] on the 1060 are A0-A5 representing pen nib, pen button 1, and pen button 2 respectively or F0 representing an aux button being pressed.

--- OpenTabletDriver.Configurations/Parsers/XP_Pen/XP_PenReportParser.cs
+++ OpenTabletDriver.Configurations/Parsers/XP_Pen/XP_PenReportParser.cs
@@ -6,7 +6,9 @@ namespace OpenTabletDriver.Configurations.Parsers.XP_Pen
     {
         public IDeviceReport Parse(byte[] report)
         {
-            if (report[1] == 0xC0)
+            if (report[1] == 0xC0 || report[1] == 0xF2)
+                // Report[1] == 0xF2 seems to be some sort of an error state for
+                // UGEE S1060W Specifically the string 02 F2 01 64 01
                 return new OutOfRangeReport(report);
 
             if (report[1].IsBitSet(4))
