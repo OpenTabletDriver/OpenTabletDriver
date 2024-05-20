6ee8291889358b8df4537644ca14464c306428d2
Spencer Sawyer <S@spswy.com>
2024-05-20 07:39:25 -0700

    Lint.

--- OpenTabletDriver.Configurations/Parsers/Veikk/VeikkAuxV1Report.cs
+++ OpenTabletDriver.Configurations/Parsers/Veikk/VeikkAuxV1Report.cs
@@ -10,25 +10,24 @@ namespace OpenTabletDriver.Configurations.Parsers.Veikk
         {
             switch (buttonId)
             {
-                default : return -1;
-                case 0x3E : return 0;
-                case 0x0C : return 1;
-                case 0x2C : return 2;
+                default: return -1;
+                case 0x3E: return 0;
+                case 0x0C: return 1;
+                case 0x2C: return 2;
                 // Buttons 4 and 6 could shadow each other in the case of more than one buttons being pressed at once.
                 // Button 6 would take precedence.
                 case 0x19:
                 {
-                    if (report1==0x00) return 3;
+                    if (report1 == 0x00) return 3;
                     return 5;
-
                 }
-                case 0x06 : return 4;
-                case 0x1D : return 6;
-                case 0x16 : return 7;
-                case 0x2E : return 8;
-                case 0x2D : return 9;
-                case 0x30 : return 10;
-                case 0x2F : return 11;
+                case 0x06: return 4;
+                case 0x1D: return 6;
+                case 0x16: return 7;
+                case 0x2E: return 8;
+                case 0x2D: return 9;
+                case 0x30: return 10;
+                case 0x2F: return 11;
             }
 
         }
