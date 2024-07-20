d4fd7991e231d584946f540744a912bfac0cfb3a
InfinityGhost <infinity@opentabletdriver.net>
2024-07-20 17:34:16 -0400

    Update to .NET 8.0

--- default.nix
+++ default.nix
@@ -32,10 +32,10 @@ buildDotnetModule rec {
 
   src = ./.;
 
-  dotnet-sdk = dotnetCorePackages.sdk_7_0;
-  dotnet-runtime = dotnetCorePackages.runtime_7_0;
+  dotnet-sdk = dotnetCorePackages.sdk_8_0;
+  dotnet-runtime = dotnetCorePackages.runtime_8_0;
 
-  dotnetInstallFlags = [ "--framework=net7.0" ];
+  dotnetInstallFlags = [ "--framework=net8.0" ];
 
   projectFile = [ "OpenTabletDriver.Console" "OpenTabletDriver.Daemon" "OpenTabletDriver.UX.Gtk" ];
   nugetDeps = ./deps.nix;
