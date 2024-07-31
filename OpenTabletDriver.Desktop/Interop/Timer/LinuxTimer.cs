2f5cfa92ac801d2a210bac35ac7a188e3e1f2381
William Horvath <william@horvath.blog>
2024-07-30 20:15:44 -0700

    LinuxTimer: replace librt timer_ with libc timerfd_ + epoll
    The old implementation signalled each timer expiration by creating new threads,
    which starves ananicy-cpp of time to keep up with any new processes started
    after OpenTabletDriver.
    
    This conflict only occurred if there were any filters enabled which required
    use of the Linux timer, and the new implementation resolves it
    (along with being more efficient in general).
    
    Signed-off-by: William Horvath <william@horvath.blog>

--- OpenTabletDriver.Desktop/Interop/Timer/LinuxTimer.cs
+++ OpenTabletDriver.Desktop/Interop/Timer/LinuxTimer.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Linq.Expressions;
 using System.Runtime.InteropServices;
 using OpenTabletDriver.Native.Linux;
 using OpenTabletDriver.Native.Linux.Timers;
 using OpenTabletDriver.Native.Linux.Timers.Structs;
 using OpenTabletDriver.Plugin;
 using OpenTabletDriver.Plugin.Timers;
+using ITimer = OpenTabletDriver.Plugin.Timers.ITimer;
+using TimerCallback = OpenTabletDriver.Native.Linux.Timers.TimerCallback;
 
 namespace OpenTabletDriver.Desktop.Interop.Timer
 {
@@ -12,18 +15,20 @@ namespace OpenTabletDriver.Desktop.Interop.Timer
 
     internal class LinuxTimer : ITimer, IDisposable
     {
+        public const int EPOLL_ID = 0x001; // hardcode an ID
         public LinuxTimer()
         {
             callbackDelegate = Callback;
             callbackHandle = GCHandle.Alloc(callbackDelegate);
-        }
 
-        private IntPtr timerID;
+        }
+        private System.Threading.Thread timerThread;
+        private int timerFD;
+        private int epollFD;
         private readonly TimerCallback callbackDelegate;
         private GCHandle callbackHandle;
         private readonly object stateLock = new object();
         private TimerSpec timeSpec;
-        private SigEvent sigEvent;
 
         public bool Enabled { private set; get; }
         public float Interval { set; get; } = 1;
@@ -36,22 +41,17 @@ namespace OpenTabletDriver.Desktop.Interop.Timer
             {
                 if (!Enabled)
                 {
-                    sigEvent = new SigEvent
-                    {
-                        notify = SigEv.Thread,
-                        thread = new SigEvThread
-                        {
-                            function = Marshal.GetFunctionPointerForDelegate(callbackDelegate),
-                            attribute = IntPtr.Zero
-                        },
-                        value = new SigVal()
-                    };
+                    // Create a timerfd instance
+                    timerFD = TimerCreate(ClockID.Monotonic, TimerFlag.TFD_NONBLOCK);
 
-                    if (TimerCreate(ClockID.Monotonic, ref sigEvent, out timerID) != ERRNO.NONE)
+                    if (timerFD == -1)
                     {
-                        Log.Write("LinuxTimer", $"Failed creating timer: {(ERRNO)Marshal.GetLastWin32Error()}", LogLevel.Error);
+                        Log.Write("LinuxTimer", $"Failed creating timer: ${(ERRNO)Marshal.GetLastWin32Error()}", LogLevel.Error);
+                        Stop();
                         return;
                     }
+
+                    Log.Write("LinuxTimer", $"timerFD: {timerFD}", LogLevel.Debug);
 
                     double interval = Interval * 1000 * 1000;
 
@@ -69,14 +69,91 @@ namespace OpenTabletDriver.Desktop.Interop.Timer
                         }
                     };
 
+                    // Arm the timer
                     var oldTimeSpec = new TimerSpec();
-                    if (TimerSetTime(timerID, TimerFlag.Default, ref timeSpec, ref oldTimeSpec) != ERRNO.NONE)
+                    if (TimerSetTime(timerFD, TimerFlag.Default, ref timeSpec, ref oldTimeSpec) != ERRNO.NONE)
                     {
                         Log.Write("LinuxTimer", $"Failed activating the timer: ${(ERRNO)Marshal.GetLastWin32Error()}", LogLevel.Error);
+                        Stop();
                         return;
                     }
 
+                    // Create an epoll instance
+                    epollFD = EpollCreate();
+                    if (epollFD == -1)
+                    {
+                        Log.Write("LinuxTimer", $"Error creating epoll instance: ${(ERRNO)Marshal.GetLastWin32Error()}", LogLevel.Error);
+                        Stop();
+                        return;
+                    }
+
+                    // Add the timer file descriptor to the epoll instance
+                    EpollEvent epollInstance = new EpollEvent
+                    {
+                        events = EPOLL_ID,
+                        fd = timerFD
+                    };
+
+                    if (EpollAdd(epollFD, timerFD, ref epollInstance) != ERRNO.NONE)
+                    {
+                        Log.Write("LinuxTimer", $"Error adding file descriptor to the epoll instance: ${(ERRNO)Marshal.GetLastWin32Error()}", LogLevel.Error);
+                        Stop();
+                        return;
+                    }
+
+                    EpollEvent epollReadEvs = new EpollEvent();
+
+                    timerThread = new System.Threading.Thread(() =>
+                    {
+                        try
+                        {
+                            int epollResult;
+                            ulong timerExpirations = 0;
+
+                            // Start the epoll event loop
+                            while (Enabled)
+                            {
+                                if (EpollWait(epollFD, out epollReadEvs, 1, -1) == -1)
+                                {
+                                    Log.Write("LinuxTimer", $"Error waiting for epoll events", LogLevel.Error);
+                                    Stop();
+                                    return;
+                                }
+
+                                if ((epollReadEvs.events & EPOLL_ID) != 0)
+                                {
+                                    epollResult = TimerGetTime(timerFD, ref timerExpirations, sizeof(ulong));
+                                    if (epollResult == -1)
+                                    {
+                                        Log.Write("LinuxTimer", $"Error reading from timer FD", LogLevel.Error);
+                                        Stop();
+                                        return;
+                                    }
+
+                                    // Update state for each timer expiration
+                                    for (ulong i = 0; i < timerExpirations; i++)
+                                    {
+                                        callbackDelegate.Invoke();
+                                        //Callback();
+                                    }
+
+                                }
+                            }
+                            Log.Write("LinuxTimer", "Timer thread was stopped", LogLevel.Debug);
+                            return;
+                        }
+                        catch (Exception timerException)
+                        {
+                            Log.Write("LinuxTimer", $"Exception occurred in timer thread: ${timerException}", LogLevel.Error);
+                            Stop();
+                            return;
+                        }
+                    });
+
                     Enabled = true;
+                    timerThread.Priority = System.Threading.ThreadPriority.Highest;
+                    timerThread.Start();
+
                 }
             }
         }
@@ -85,43 +162,49 @@ namespace OpenTabletDriver.Desktop.Interop.Timer
         {
             lock (stateLock)
             {
-                if (Enabled)
+                Enabled = false;
+                try
                 {
-                    var timeSpec = new TimerSpec
-                    {
-                        interval = new TimeSpec
-                        {
-                            sec = 0,
-                            nsec = 0
-                        }
-                    };
+                    timerThread.Join();
 
-                    if (TimerSetTime(timerID, TimerFlag.Default, ref timeSpec, IntPtr.Zero) != ERRNO.NONE)
+                    if (EpollRemove(epollFD, timerFD) != ERRNO.NONE)
                     {
-                        Log.Write("LinuxTimer", $"Failed deactivating the timer: ${(ERRNO)Marshal.GetLastWin32Error()}", LogLevel.Error);
-                        return;
+                        Log.Write("LinuxTimer",
+                                $"Could not remove the timer from the epoll instance: ${(ERRNO)Marshal.GetLastWin32Error()}",
+                                LogLevel.Debug);
                     }
 
-                    if (TimerDelete(timerID) != ERRNO.NONE)
+                    if (CloseFileDescriptor(epollFD) != ERRNO.NONE)
                     {
-                        Log.Write("LinuxTimer", $"Failed deleting the timer: ${(ERRNO)Marshal.GetLastWin32Error()}", LogLevel.Error);
-                        return;
+                        Log.Write("LinuxTimer",
+                                $"Failed closing the epoll file descriptor: ${(ERRNO)Marshal.GetLastWin32Error()}",
+                                LogLevel.Debug);
                     }
 
-                    Enabled = false;
+                    if (CloseFileDescriptor(timerFD) != ERRNO.NONE)
+                    {
+                        Log.Write("LinuxTimer",
+                                $"Failed closing the timer file descriptor: ${(ERRNO)Marshal.GetLastWin32Error()}",
+                                LogLevel.Debug);
+                    }
+                    Log.Write("LinuxTimer",
+                              "Finished cleaning up",
+                              LogLevel.Debug);
+
+                    return;
                 }
+                catch { }
             }
         }
 
-        private void Callback(SigVal _)
+        private void Callback()
         {
             Elapsed?.Invoke();
         }
 
         public void Dispose()
         {
-            if (Enabled)
-                Stop();
+            Stop();
             callbackHandle.Free();
             GC.SuppressFinalize(this);
         }
--- OpenTabletDriver.Native/Linux/Timers/TimerFlag.cs
+++ OpenTabletDriver.Native/Linux/Timers/TimerFlag.cs
@@ -3,6 +3,7 @@ namespace OpenTabletDriver.Native.Linux.Timers
     public enum TimerFlag
     {
         Default,
-        AbsoluteTime
+        AbsoluteTime,
+        TFD_NONBLOCK = 2048 // open timer file descriptor as non-blocking
     }
 }
