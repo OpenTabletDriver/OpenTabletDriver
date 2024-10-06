4a26bf9c86b37d577bc8be02efe41e6332c3ea9a
BunnyTub <51249136+BunnyTub@users.noreply.github.com>
2024-10-06 05:28:38 -0400

    Prevent multiple message boxes from opening at once (spamming)
    Uses the MessageBoxActive bool to determine if there is a window already open.

--- OpenTabletDriver.UX/Extensions.cs
+++ OpenTabletDriver.UX/Extensions.cs
@@ -12,31 +12,41 @@ namespace OpenTabletDriver.UX
 {
     public static class Extensions
     {
+        private static bool MessageBoxActive = false;
+
         public static void ShowMessageBox(this Exception exception)
         {
+            if (MessageBoxActive)
+                return;
             Application.Instance.Invoke(() =>
             {
                 exception = exception.GetBaseException();
 
                 var dialog = new ExceptionDialog(exception);
+                MessageBoxActive = true;
                 dialog.ShowModal(Application.Instance.MainForm);
+                MessageBoxActive = false;
             });
         }
 
         public static void ShowMessageBox(this CommonErrorData errorData)
         {
+            if (MessageBoxActive)
+                return;
             string message = errorData.Message + Environment.NewLine + errorData.StackTrace;
             Log.Write(
                 errorData.TypeName,
                 message,
                 LogLevel.Error
             );
+            MessageBoxActive = true;
             MessageBox.Show(
                 message,
                 errorData.TypeName,
                 MessageBoxButtons.OK,
                 MessageBoxType.Error
             );
+            MessageBoxActive = false;
         }
 
         public static BindableBinding<TControl, bool> GetEnabledBinding<TControl>(this TControl control) where TControl : Control
