6f9635b07c949587683a2792f467d847c636f527
Gess1t <tintamare91@gmail.com>
2024-11-13 21:35:00 +0100

    Fix wheel spec using the wrong attribute for 0.6.x

--- OpenTabletDriver.Plugin/Tablet/WheelSpecifications.cs
+++ OpenTabletDriver.Plugin/Tablet/WheelSpecifications.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace OpenTabletDriver.Plugin.Tablet
 {
@@ -10,24 +11,26 @@ namespace OpenTabletDriver.Plugin.Tablet
         /// <summary>
         /// The amount of steps making up a whole rotation.
         /// </summary>
-        [DisplayName("Steps")]
+        [Required(ErrorMessage = $"{nameof(StepCount)} must be defined")]
         public uint StepCount { set; get; }
 
         /// <summary>
         /// Does the wheel report relative position (movement) or absolute position
         /// </summary>
-        [DisplayName("Relative")]
+        [Required(ErrorMessage = $"{nameof(IsRelative)} must be defined")]
         public bool IsRelative { get; set; }
 
         /// <summary>
         /// Does a increment/positive movement indicate clockwise movement?
         /// </summary>
-        [DisplayName("Clockwise")]
+        [Required(ErrorMessage = $"{nameof(IsClockwise)} must be defined")]
         public bool IsClockwise { get; set; }
 
         /// <summary>
         /// For absolute wheels, the angle on the unit circle, corresponding to a reading of zero from the sensor
         /// </summary>
+        [Required(ErrorMessage = $"{nameof(AngleOfZeroReading)} must be defined")]
+        [Range(0, 360)]
         public float AngleOfZeroReading { get; set; }
     }
 }
