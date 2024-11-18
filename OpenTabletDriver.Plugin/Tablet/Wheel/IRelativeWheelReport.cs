195be24886730f9145b46a5a4e8e0f883470f0ba
Gess1t <39861216+Mrcubix@users.noreply.github.com>
2024-11-18 21:03:17 +0100

    Make Wheel Delta nullable again & change its doc again
    Nullable means it's not in use
    
    Co-authored-by: Johannes Elgaard <joha4270@gmail.com>

--- OpenTabletDriver.Plugin/Tablet/Wheel/IRelativeWheelReport.cs
+++ OpenTabletDriver.Plugin/Tablet/Wheel/IRelativeWheelReport.cs
@@ -6,8 +6,8 @@ namespace OpenTabletDriver.Plugin.Tablet.Wheel
     public interface IRelativeWheelReport : IDeviceReport
     {
         /// <summary>
-        /// The relative wheel movement from its previous state, or 0 to indicate an absence of touch.
+        /// The relative wheel movement from its previous state, 0 to indicate an absence of movement or null for an absence of touch.
         /// </summary>
-        int WheelDelta { get; set; }
+        int? WheelDelta { get; set; }
     }
 }
