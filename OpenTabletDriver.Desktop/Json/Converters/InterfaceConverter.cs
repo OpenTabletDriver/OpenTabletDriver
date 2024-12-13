4e91cbaab4396d1af0e81d5772ae2a1528f61df8
Aki Sakurai <75532970+AkiSakurai@users.noreply.github.com>
2024-12-13 08:16:17 +0800

    Update OpenTabletDriver.Desktop/Json/Converters/InterfaceConverter.cs
    Co-authored-by: Sebastian Jensen <gonx@gonx.dk>

--- OpenTabletDriver.Desktop/Json/Converters/InterfaceConverter.cs
+++ OpenTabletDriver.Desktop/Json/Converters/InterfaceConverter.cs
@@ -5,6 +5,9 @@ namespace OpenTabletDriver.Desktop.Json.Converters
 {
     public class InterfaceConverter<TInterface, TClass> : JsonConverter<TInterface> where TInterface : class where TClass : TInterface, new()
     {
+        // Newtonsoft.JSON custom `JsonConverter`s that only implement deserialization
+        //      must specify CanWrite = false to use their default JSON writer
+        // See upstream issue: https://github.com/JamesNK/Newtonsoft.Json/issues/2432
         public override bool CanWrite => false;
 
         public override void WriteJson(JsonWriter writer, TInterface? value, JsonSerializer serializer)
