b88c558e652af9638fd521afaff7a6c2cf5ce003
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2025-01-31 22:02:38 +0100

    eng/lib: Fix COMMIT_DISTANCE_FROM_TAG support of the new VERSION_SUFFIX format
    Should be backported to https://github.com/OpenTabletDriver/OpenTabletDriver/pull/3745

--- eng/lib.sh
+++ eng/lib.sh
@@ -18,7 +18,7 @@ if [ -z "$VERSION_SUFFIX" ]; then
     VERSION_SUFFIX="-$(git describe --long --tags --dirty | sed -E 's/^v((.\.)*.)-(.*)$/\3/')"
 
     # don't set suffix if this is a tagged commit
-    COMMIT_DISTANCE_FROM_TAG="$(sed -E 's/^([0-9]+)-(.*)$/\1/' <<< "$VERSION_SUFFIX")"
+    COMMIT_DISTANCE_FROM_TAG="$(sed -E 's/^-([0-9]+)-(.*)$/\1/' <<< "$VERSION_SUFFIX")"
     if [ "$COMMIT_DISTANCE_FROM_TAG" -eq "0" ]; then
       echo "INFO: This looks like a tagged commit, not setting a version suffix"
       unset VERSION_SUFFIX
