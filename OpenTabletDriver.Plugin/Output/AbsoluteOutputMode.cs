0ca557ff412cd674178b37086903270f7fccb7fb
Aki <75532970+AkiSakurai@users.noreply.github.com>
2025-02-04 19:39:27 +0800

    [0.6.x] Add options to disable pressure & tilt

--- OpenTabletDriver.Plugin/Output/AbsoluteOutputMode.cs
+++ OpenTabletDriver.Plugin/Output/AbsoluteOutputMode.cs
@@ -62,6 +62,16 @@ namespace OpenTabletDriver.Plugin.Output
         /// </remarks>
         public bool AreaLimiting { set; get; }
 
+        /// <summary>
+        /// Whether to disable pressure
+        /// </summary>
+        public bool DisablePressure { set; get; }
+
+        /// <summary>
+        /// Whether to disable tilt
+        /// </summary>
+        public bool DisableTilt { set; get; }
+
         protected override Matrix3x2 CreateTransformationMatrix()
         {
             if (Input != null && Output != null && Tablet != null)
@@ -143,10 +153,10 @@ namespace OpenTabletDriver.Plugin.Output
                 hoverDistanceHandler.SetHoverDistance(proximityReport.HoverDistance);
             if (report is IEraserReport eraserReport && Pointer is IEraserHandler eraserHandler)
                 eraserHandler.SetEraser(eraserReport.Eraser);
-            if (report is ITiltReport tiltReport && Pointer is ITiltHandler tiltHandler)
+            if (report is ITiltReport tiltReport && Pointer is ITiltHandler tiltHandler && !DisableTilt)
                 tiltHandler.SetTilt(tiltReport.Tilt);
-            if (report is ITabletReport tabletReport && Pointer is IPressureHandler pressureHandler
-                && Tablet?.Properties.Specifications.Pen != null)
+            if (report is ITabletReport tabletReport && Pointer is IPressureHandler pressureHandler &&
+                !DisablePressure && Tablet?.Properties.Specifications.Pen != null)
                 pressureHandler.SetPressure(tabletReport.Pressure / (float)Tablet.Properties.Specifications.Pen.MaxPressure);
 
             // make sure to set the position last
--- OpenTabletDriver.Plugin/Output/RelativeOutputMode.cs
+++ OpenTabletDriver.Plugin/Output/RelativeOutputMode.cs
@@ -75,6 +75,16 @@ namespace OpenTabletDriver.Plugin.Output
             get => _resetTime;
         }
 
+        /// <summary>
+        /// Whether to disable pressure
+        /// </summary>
+        public bool DisablePressure { set; get; }
+
+        /// <summary>
+        /// Whether to disable tilt
+        /// </summary>
+        public bool DisableTilt { set; get; }
+
         protected override Matrix3x2 CreateTransformationMatrix()
         {
             var transform = Matrix3x2.CreateRotation(
@@ -144,10 +154,10 @@ namespace OpenTabletDriver.Plugin.Output
                 hoverDistanceHandler.SetHoverDistance(proximityReport.HoverDistance);
             if (report is IEraserReport eraserReport && Pointer is IEraserHandler eraserHandler)
                 eraserHandler.SetEraser(eraserReport.Eraser);
-            if (report is ITiltReport tiltReport && Pointer is ITiltHandler tiltHandler)
+            if (report is ITiltReport tiltReport && Pointer is ITiltHandler tiltHandler && !DisableTilt)
                 tiltHandler.SetTilt(tiltReport.Tilt);
-            if (report is ITabletReport tabletReport && Pointer is IPressureHandler pressureHandler
-                && Tablet?.Properties.Specifications.Pen != null)
+            if (report is ITabletReport tabletReport && Pointer is IPressureHandler pressureHandler &&
+                !DisablePressure && Tablet?.Properties.Specifications.Pen != null)
                 pressureHandler.SetPressure(tabletReport.Pressure / (float)Tablet.Properties.Specifications.Pen.MaxPressure);
 
             // make sure to set the position last
