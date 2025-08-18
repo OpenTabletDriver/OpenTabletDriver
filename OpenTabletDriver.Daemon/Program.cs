765daa11e650f2bde563d5aff4c3b9bfcc997ae5
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2025-08-18 22:29:12 +0200

    Daemon: Handle termination signals
    SIGINT/SIGTERM/SIGHUP etc would cause the daemon to not properly clean up the named pipes, causing issues on multi-user Linux systems.
    
    This commit uses a cancelation token to instruct the daemon to abort taking further connections, and then the handler waits for the daemon to finish

--- OpenTabletDriver.Daemon/Program.cs
+++ OpenTabletDriver.Daemon/Program.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.CommandLine;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Loader;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
@@ -78,35 +81,100 @@ namespace OpenTabletDriver.Daemon
                 Environment.Exit(ret);
             }
 
-            using (var instance = new Instance("OpenTabletDriver.Daemon"))
+            await StartDaemon();
+        }
+
+        static async Task StartDaemon()
+        {
+            using var instance = new Instance("OpenTabletDriver.Daemon");
+            if (instance.AlreadyExists)
             {
-                if (instance.AlreadyExists)
-                {
-                    Console.WriteLine("OpenTabletDriver Daemon is already running.");
-                    Thread.Sleep(1000);
-                    return;
-                }
-
-                TaskScheduler.UnobservedTaskException += (_, e) =>
-                {
-                    Log.Exception(e.Exception);
-                    e.SetObserved();
-                };
-
-                var host = new RpcHost<DriverDaemon>("OpenTabletDriver.Daemon");
-                host.ConnectionStateChanged += (sender, state) =>
-                    Log.Write("IPC", $"{(state ? "Connected to" : "Disconnected from")} a client.", LogLevel.Debug);
-
-                try
-                {
-                    await host.Run(BuildDaemon());
-                }
-                catch (Exception e)
-                {
-                    Log.Exception(e, LogLevel.Fatal);
-                    Environment.Exit(1);
-                }
+                Console.WriteLine("OpenTabletDriver Daemon is already running.");
+                Thread.Sleep(1000);
+                return;
             }
+
+            TaskScheduler.UnobservedTaskException += (_, e) =>
+            {
+                Log.Exception(e.Exception);
+                e.SetObserved();
+            };
+
+            var host = GetRpcHost();
+
+            var cts = new CancellationTokenSource();
+            bool tokenCancelled = false;
+            bool daemonRunning = false;
+
+            // Handle SIGINT
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true; // must set to true otherwise we can't wait gracefully
+                Log.Debug("CancelKeyPressHandler", "Handling Ctrl+C/SIGINT");
+                CloseDaemon();
+            };
+
+            // Handle SIGHUP
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                PosixSignal[] trappedSignals = [PosixSignal.SIGHUP];
+
+                foreach (var signal in trappedSignals)
+                    PosixSignalRegistration.Create(signal,
+                        [SuppressMessage("ReSharper", "AccessToModifiedClosure")](_) =>
+                        {
+                            Log.Debug("signal", Enum.GetName(signal));
+                            CloseDaemon();
+                        });
+            }
+
+            // Handle other termination operations (SIGTERM, etc.)
+            AssemblyLoadContext.Default.Unloading += _ =>
+            {
+                Log.Write("AssemblyLoadContext.Unloading", "Terminating daemon..");
+                CloseDaemon();
+            };
+
+            try
+            {
+                daemonRunning = true;
+                await host.Run(BuildDaemon(), cts.Token);
+                daemonRunning = false;
+                Log.Write("ProgramMain","Daemon gracefully stopped");
+            }
+            catch (Exception e)
+            {
+                Log.Exception(e, LogLevel.Fatal);
+                Environment.Exit(1);
+            }
+
+            return;
+
+            void CloseDaemon()
+            {
+                if (tokenCancelled) // don't cancel twice
+                    return;
+
+                tokenCancelled = true;
+                cts.CancelAsync();
+
+                Log.Write("closeDaemon", "Waiting for daemon to terminate");
+
+                // ReSharper disable once LoopVariableIsNeverChangedInsideLoop
+                // ReSharper disable once AccessToModifiedClosure
+                while (daemonRunning)
+                    Thread.Sleep(100);
+
+                Log.Write("closeDaemon", "Waiting for daemon finished");
+            }
+        }
+
+        private static RpcHost<DriverDaemon> GetRpcHost()
+        {
+            var host = new RpcHost<DriverDaemon>("OpenTabletDriver.Daemon");
+            host.ConnectionStateChanged += (sender, state) =>
+                Log.Write("IPC", $"{(state ? "Connected to" : "Disconnected from")} a client.", LogLevel.Debug);
+            return host;
         }
 
         static CommandLineOptions ParseCmdLineOptions(string[] args)
