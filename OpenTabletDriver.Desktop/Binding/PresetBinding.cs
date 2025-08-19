05975987d8cdd0c73fb15b0bfbcb257b7bcd9199
Gess1t <tintamare91@gmail.com>
2025-08-19 21:13:46 +0200

    Add timeout to prevent an issue with pen buttons

--- OpenTabletDriver.Desktop/Binding/PresetBinding.cs
+++ OpenTabletDriver.Desktop/Binding/PresetBinding.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using OpenTabletDriver.Desktop.Contracts;
@@ -5,12 +6,16 @@ using OpenTabletDriver.Plugin;
 using OpenTabletDriver.Plugin.Attributes;
 using OpenTabletDriver.Plugin.DependencyInjection;
 using OpenTabletDriver.Plugin.Tablet;
+using OpenTabletDriver.Plugin.Timing;
 
 namespace OpenTabletDriver.Desktop.Binding
 {
     [PluginName("Preset Binding")]
     public class PresetBinding : IStateBinding
     {
+        private const int TIMEOUT = 50;
+        private readonly static HPETDeltaStopwatch _stopwatch = new();
+
         public readonly static IReadOnlyCollection<Preset> Presets = AppInfo.PresetManager.GetPresets();
         public static string[] ValidPresets => Presets.Select(x => x.Name).ToArray();
 
@@ -22,7 +27,7 @@ namespace OpenTabletDriver.Desktop.Binding
 
         public void Press(TabletReference tablet, IDeviceReport report)
         {
-            if (Preset != null && AppInfo.PresetManager != null)
+            if (Preset != null && AppInfo.PresetManager != null && _stopwatch.Elapsed.Milliseconds > TIMEOUT)
             {
                 // Force a refresh, preset list may be out of date
                 AppInfo.PresetManager.Refresh();
@@ -38,6 +43,8 @@ namespace OpenTabletDriver.Desktop.Binding
                 else
                     Log.Write("Settings", $"Failed to find preset '{Preset}' or daemon is null.");
             }
+
+            _stopwatch.Restart();
         }
 
         public void Release(TabletReference tablet, IDeviceReport report) { }
