c319fda9547aa5cab722620194c6574740cccd98
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2025-08-27 12:25:18 +0200

    AreaConverter: Add Gaomon 2024 converter

---
+++ OpenTabletDriver.Desktop/Conversion/GaomonV2AreaConverter.cs
@@ -0,0 +1,30 @@
+using System.Numerics;
+using OpenTabletDriver.Plugin;
+using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Tablet;
+
+namespace OpenTabletDriver.Desktop.Conversion
+{
+    [PluginName("Gaomon, 2024 and newer")]
+    public class GaomonV2AreaConverter : IAreaConverter
+    {
+        public virtual DeviceVendor Vendor => DeviceVendor.Gaomon;
+
+        public string Top => "Width";
+        public string Left => "Height";
+        public string Bottom => "X";
+        public string Right => "Y";
+
+        public Area Convert(TabletReference tablet, double iWidth, double iHeight, double x, double y)
+        {
+            var digitizer = tablet.Properties.Specifications.Digitizer;
+
+            var oWidth = (iWidth / digitizer.MaxX) * digitizer.Width;
+            var oHeight =  (iHeight / digitizer.MaxY) * digitizer.Height;
+            var offsetX = (x / digitizer.MaxX) * digitizer.Width + (oWidth / 2);
+            var offsetY = (x / digitizer.MaxY) * digitizer.Height + (oHeight / 2);
+
+            return new Area((float)oWidth, (float)oHeight, new Vector2((float)offsetX, (float)offsetY), 0f);
+        }
+    }
+}
