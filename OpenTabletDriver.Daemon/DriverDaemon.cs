0b7bbfe1aa837f45c5c49ce0818a08a9215a8f00
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2025-08-30 03:22:48 +0200

    Add IPenActionHandler and add to Binding Service Provider
    This is to facilitate an upcoming 'adaptive bindings' feature
    
    Alternatively, we would have had to use `IMouseHandler` which would
    cause some likely undesirable side effects

--- OpenTabletDriver.Daemon/DriverDaemon.cs
+++ OpenTabletDriver.Daemon/DriverDaemon.cs
@@ -396,6 +396,9 @@ namespace OpenTabletDriver.Daemon
             if (pointer is IMouseButtonHandler mouseButtonHandler)
                 bindingServiceProvider.AddService(() => mouseButtonHandler);
 
+            if (pointer is IPenActionHandler penActionHandler)
+                bindingServiceProvider.AddService(() => penActionHandler);
+
             var tip = bindingHandler.Tip = new ThresholdBindingState
             {
                 Binding = settings.TipButton?.Construct<IBinding>(bindingServiceProvider, tabletReference),
