21c093902282e8ca1f64cd72ee283ad8ae556125
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2025-09-02 21:54:57 +0200

    TabletConfiguration: Migrate to new configuration format
    Notably, this fixes the spelling mistake 'Auxilary' to 'Auxiliary'
    
    Additionally, the Buttons PenSpecification property has been moved directly into a ButtonCount uint
    
    This is backwards-compatible with old configurations, but new configurations should use the new format where possible

---
+++ OpenTabletDriver.Plugin/Globals.cs
@@ -0,0 +1,8 @@
+namespace OpenTabletDriver.Plugin
+{
+    public static class Globals
+    {
+        public const string LegacyTabletConfigurationProperty =
+            "Property used in older configurations. Only to be used for testing purposes.";
+    }
+}
