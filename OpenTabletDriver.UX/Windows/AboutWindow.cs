eff13632236786a47b85c9a2516f3454dfeab665
kuuuube <hexagonisalie@gmail.com>
2025-09-21 23:24:10 -0400

    Rewrite about dialog

---
+++ OpenTabletDriver.UX/Windows/AboutWindow.cs
@@ -0,0 +1,81 @@
+using System;
+using Eto.Drawing;
+using Eto.Forms;
+
+namespace OpenTabletDriver.UX.Windows
+{
+    public class AboutWindow : DesktopForm
+    {
+        const int LARGE_FONTSIZE = 14;
+        const int FONTSIZE = LARGE_FONTSIZE - 4;
+        const int SPACING = 10;
+
+        private readonly string[] Developers = ["InfinityGhost", "X9VoiD", "gonX", "jamesbt365", "Kuuube", "AkiSakurai"];
+        private readonly string[] Designers = ["InfinityGhost"];
+        private readonly string[] Documenters = ["InfinityGhost", "gonX", "jamesbt365", "Kuuube"];
+
+        public AboutWindow()
+            : base(Application.Instance.MainForm)
+        {
+            Title = "About OpenTabletDriver";
+
+            var aboutTabContent = new StackLayout
+            {
+                HorizontalContentAlignment = HorizontalAlignment.Center,
+                VerticalContentAlignment = VerticalAlignment.Stretch,
+                Width = 500,
+                Padding = SPACING,
+                Spacing = SPACING / 2,
+                Items =
+                {
+                    new ImageView
+                    {
+                        Image = new Bitmap(App.Logo.WithSize(256, 256)),
+                    },
+                    new Label
+                    {
+                        Text = "OpenTabletDriver",
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Font = SystemFonts.Bold(FONTSIZE),
+                    },
+                    new Label
+                    {
+                        Text = $"v{App.Version}",
+                        VerticalAlignment = VerticalAlignment.Center,
+                    },
+                    new Label
+                    {
+                        Text = "Open source, cross-platform tablet configurator",
+                        VerticalAlignment = VerticalAlignment.Center,
+                    },
+                    new LinkButton
+                    {
+                        Text = "OpenTabletDriver Github Repository",
+                        Command = new Command((s, e) => Application.Instance.Open(App.Website.ToString())),
+                    }
+                }
+            };
+
+            var creditsTabContent = new TextArea
+            {
+                ReadOnly = true,
+                Text = "Developers:" + Environment.NewLine + string.Join(Environment.NewLine, Developers) + Environment.NewLine + Environment.NewLine
+                        + "Developers:" + Environment.NewLine + string.Join(Environment.NewLine, Designers) + Environment.NewLine + Environment.NewLine
+                        + "Developers:" + Environment.NewLine + string.Join(Environment.NewLine, Documenters)
+            };
+
+            var licenseTabContent = new TextArea
+            {
+                ReadOnly = true,
+                Text = App.License,
+            };
+
+            var tabControl = new TabControl();
+            tabControl.Pages.Add(new TabPage(aboutTabContent) { Text = "About" });
+            tabControl.Pages.Add(new TabPage(creditsTabContent) { Text = "Credits" });
+            tabControl.Pages.Add(new TabPage(licenseTabContent) { Text = "License" });
+
+            this.Content = tabControl;
+        }
+    }
+}
