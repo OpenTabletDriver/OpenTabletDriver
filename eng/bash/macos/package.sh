2d2159600b72cd029b0de1f2c79372c12d294cea
Aki <75532970+AkiSakurai@users.noreply.github.com>
2025-10-17 20:39:07 +0800

    [0.6.x] MacOS: Add native ARM64 build and release support     This commit introduces comprehensive support for building, testing, and releasing OpenTabletDriver natively for macOS on the ARM64 architecture (Apple Silicon).     Key changes include:     CI/CD:
        Added a arm64 job to dotnet.yml to build and upload ARM64 artifacts.
    
        Build System:
    
        The script performs ad-hoc codesigning on the application bundle for both ARM64 and x64 binaries.
        Codesigning is required for ARM64 binaries to run on Apple Silicon but is not required for x64 binaries; however, codesigning is applied to both for consistency.
    
        An earlier version of Eto had a bug where the automatic bundle was not generated when an output folder was specified.
        This bug has been fixed in Eto version 2.10.1, which now generates the bundle automatically.
        Since we manually create the application bundle, this commit disables Eto's automatic .app creation during publishing in the build script to prevent nested bundle creation.
    
        Project Configuration:
    
        Upgraded Eto.Platform.Mac64 to version 2.10.1.
        This version includes critical fixes for UI rendering, stability, universal binary compatibility on ARM64, and resolves the issue with extraneous bundle creation.
    
        Updated OpenTabletDriver.UX.MacOS.csproj to include resources like the app icon and Info.plist for Eto's automatic bundle generation.
    
        Added UseCurrentRuntimeIdentifier to the project configuration to allow Rider to build and execute using the host's architecture.

--- eng/bash/macos/package.sh
+++ eng/bash/macos/package.sh
@@ -17,6 +17,13 @@ mkdir -p "${pkg_root}/Contents/Resources"
 cp "${pkg_script_root}/Icon.icns" "${pkg_root}/Contents/Resources/"
 cp "${pkg_script_root}/Info.plist" "${pkg_root}/Contents/"
 
+if type -p codesign; then
+    echo "Signing ${NET_RUNTIME} MacOS App..."
+    codesign  -s - -f --deep "${pkg_root}"
+else
+    echo "Skipping signing ${NET_RUNTIME} MacOS App..."
+fi
+
 echo "Creating tarball..."
 create_binary_tarball "${pkg_root}" "${OUTPUT}/${PKG_FILE}"
 
