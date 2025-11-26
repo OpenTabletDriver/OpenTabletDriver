efefca516e215e132bc692700e8936cc5ccb7142
Jason Beck <mrbass21@gmail.com>
2025-11-25 22:30:31 -0600

    Refactor to use GianoReportParser and rename wheel with rel in name

--- OpenTabletDriver.Configurations/Parsers/Huion/KamvasRelWheelReport.cs
+++ OpenTabletDriver.Configurations/Parsers/Huion/KamvasRelWheelReport.cs
@@ -2,14 +2,12 @@ using OpenTabletDriver.Plugin.Tablet.Wheel;
 
 namespace OpenTabletDriver.Configurations.Parsers.Huion;
 
-public class KamvasWheelReport : IRelativeWheelReport
+public class KamvasRelWheelReport : IRelativeWheelReport
 {
-    public KamvasWheelReport(byte[] data)
+    public KamvasRelWheelReport(byte[] data)
     {
-        if(data == null)
-        {
+        if (data == null)
             return;
-        }
         Raw = data;
 
         // TODO: Handle when multi-wheel support is added
@@ -17,23 +15,17 @@ public class KamvasWheelReport : IRelativeWheelReport
         {
             // Wheel 1 moved
             case 1:
-                if (data[5] == 1)
-                {
+                if (data[5] == 0x1)
                     Delta = 1;
-                } else if (data[5] == 2)
-                {
+                else if (data[5] == 0x2)
                     Delta = -1;
-                }
                 break;
             // Wheel 2 moved
             case 2:
-                if (data[5] == 1)
-                {
+                if (data[5] == 0x1)
                     Delta = 1;
-                } else if (data[5] == 2)
-                {
+                else if (data[5] == 0x2)
                     Delta = -1;
-                }
                 break;
         }
     }
