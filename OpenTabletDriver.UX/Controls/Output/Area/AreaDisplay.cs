5376aebb8d206d9e9aeea2afe9c8d1fb34e6528d
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2026-02-21 03:02:34 +0100

    fixup: nullability <i lost count>

--- OpenTabletDriver.UX/Controls/Output/Area/AreaDisplay.cs
+++ OpenTabletDriver.UX/Controls/Output/Area/AreaDisplay.cs
@@ -426,7 +426,7 @@ namespace OpenTabletDriver.UX.Controls.Output.Area
             }
         }
 
-        private void DrawText(Graphics graphics, string? errorText)
+        private void DrawText(Graphics graphics, string errorText)
         {
             ArgumentNullException.ThrowIfNull(errorText);
             if (errorText.Length == 0) return;
