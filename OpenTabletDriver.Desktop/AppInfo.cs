2d8c19d7e22251160aef41a169863b34ae7a1495
kuuuube <hexagonisalie@gmail.com>
2026-07-17 20:21:31 -0400

    Consolidate config directory setting to GetDefaultConfigurationDirectory

--- OpenTabletDriver.Desktop/AppInfo.cs
+++ OpenTabletDriver.Desktop/AppInfo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using OpenTabletDriver.Desktop.Reflection;
@@ -46,7 +47,6 @@ namespace OpenTabletDriver.Desktop
                 },
                 PluginPlatform.Linux => new AppInfo
                 {
-                    ConfigurationDirectory = GetExistingPath(Path.Join(UnixXdgPath.DataHome, "OpenTabletDriver/Configurations")),
                     AppDataDirectory = GetExistingPathOrLast(Path.Join(ProgramDirectory, "userdata"), Path.Join(UnixXdgPath.ConfigHome, "OpenTabletDriver")),
                     TemporaryDirectory = GetPath(Path.Join(UnixXdgPath.RuntimeDir, "OpenTabletDriver")),
                     CacheDirectory = GetPath(Path.Join(UnixXdgPath.CacheHome, "OpenTabletDriver")),
@@ -156,11 +156,20 @@ namespace OpenTabletDriver.Desktop
 
         public static string ProgramDirectory => AppContext.BaseDirectory;
 
-        private string GetDefaultConfigurationDirectory() => GetExistingPathOrLast(
-            Path.Join(AppDataDirectory, "Configurations"),
-            Path.Join(ProgramDirectory, "Configurations"),
-            Path.Join(Environment.CurrentDirectory, "Configurations")
-        );
+        private string GetDefaultConfigurationDirectory()
+        {
+            List<string> paths = [
+                Path.Join(AppDataDirectory, "Configurations"),
+                Path.Join(ProgramDirectory, "Configurations"),
+                Path.Join(Environment.CurrentDirectory, "Configurations")
+            ];
+            if (SystemInterop.CurrentPlatform == PluginPlatform.Linux)
+                paths.Insert(0, Path.Join(UnixXdgPath.DataHome, "OpenTabletDriver/Configurations"));
+            if (CommandLineConfigurationDirectory != null)
+                paths.Insert(0, CommandLineConfigurationDirectory);
+
+            return GetExistingPathOrLast([.. paths]);
+        }
 
         private string GetDefaultSettingsFile() => Path.Join(AppDataDirectory, "settings.json");
         private string GetDefaultPluginDirectory() => Path.Join(AppDataDirectory, "Plugins");
