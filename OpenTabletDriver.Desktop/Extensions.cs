ca631d9ee8ef8b9904d7dc73465eb8768ed0736d
Sebastian 'gonX' Jensen <gonx@gonx.dk>
2026-07-21 02:36:20 +0200

    Move new IEnumerable extensions to core project
    They will be needed for an upcoming commit

--- OpenTabletDriver.Desktop/Extensions.cs
+++ OpenTabletDriver.Desktop/Extensions.cs
@@ -24,12 +24,6 @@ namespace OpenTabletDriver.Desktop
                    select type;
         }
 
-        extension<T>(IEnumerable<T?> o) where T : class
-        {
-            public IEnumerable<T> WhereNotNull() => o.Where(x => x != null).Cast<T>();
-            public IEnumerable<TResult> SelectNotNull<TResult>(Func<T, TResult> fun) => o.WhereNotNull().Select(fun);
-        }
-
         /// <summary>
         /// Check if provided assembly is loadable.
         /// Useful to check whether an OpenTabletDriver plugin is valid or not.
--- OpenTabletDriver/Extensions.cs
+++ OpenTabletDriver/Extensions.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using OpenTabletDriver.Plugin;
 
 namespace OpenTabletDriver
 {
-    internal static class Extensions
+    public static class Extensions
     {
         public static bool TryGet<TSource, TValue>(this TSource source, Func<TSource, TValue> predicate, [NotNullWhen(true)] out TValue? value)
         {
@@ -25,5 +27,11 @@ namespace OpenTabletDriver
 
         public static TValue SafeGet<TSource, TValue>(this TSource source, Func<TSource, TValue> predicate, TValue fallback) =>
             source.TryGet(predicate, out var value) ? value : fallback;
+
+        extension<T>(IEnumerable<T?> o) where T : class
+        {
+            public IEnumerable<T> WhereNotNull() => o.Where(x => x != null).Cast<T>();
+            public IEnumerable<TResult> SelectNotNull<TResult>(Func<T, TResult> fun) => o.WhereNotNull().Select(fun);
+        }
     }
 }
