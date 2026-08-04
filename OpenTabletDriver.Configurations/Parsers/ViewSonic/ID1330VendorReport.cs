793594d7cc2f5f26ff38160a16e44135c70b8680
mirukuneko <rinnyanneko@outlook.com>
2026-08-04 22:37:09 +0800

    fix size

--- OpenTabletDriver.Configurations/Parsers/ViewSonic/ID1330VendorReport.cs
+++ OpenTabletDriver.Configurations/Parsers/ViewSonic/ID1330VendorReport.cs
@@ -6,21 +6,14 @@ namespace OpenTabletDriver.Configurations.Parsers.ViewSonic
 {
     public struct ID1330VendorReport : ITabletReport, ITiltReport, IEraserReport
     {
-        private const float VENDOR_MAX_X = 29376f;
-        private const float VENDOR_MAX_Y = 16523f;
-        private const float TABLET_MAX_X = 30000f;
-        private const float TABLET_MAX_Y = 16875f;
-
         public ID1330VendorReport(byte[] report)
         {
             Raw = report;
 
-            var vendorX = Unsafe.ReadUnaligned<ushort>(ref report[1]);
-            var vendorY = Unsafe.ReadUnaligned<ushort>(ref report[5]);
             Position = new Vector2
             {
-                X = vendorX * TABLET_MAX_X / VENDOR_MAX_X,
-                Y = vendorY * TABLET_MAX_Y / VENDOR_MAX_Y
+                X = Unsafe.ReadUnaligned<ushort>(ref report[1]),
+                Y = Unsafe.ReadUnaligned<ushort>(ref report[5])
             };
 
             var penByte = report[9];
