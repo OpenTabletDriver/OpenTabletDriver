b1bbbcff49a10b8c18e049ebe399010b797a41fc
Raquel <ela@raquellie.com>
2026-08-10 23:27:11 +0100

    Added support for the second wheel (trackpad)
    Kindly implemented by the co-author; I simply provided the packet
    captures and tested it on my device.
    Co-authored-by: Gess1t <tintamare91@gmail.com>

---
+++ OpenTabletDriver.Configurations/Parsers/XP_Pen/XP_PenReportParserV2.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics.CodeAnalysis;
+using OpenTabletDriver.Plugin.Tablet;
+
+namespace OpenTabletDriver.Configurations.Parsers.XP_Pen
+{
+    [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)]
+    public class XP_PenReportParserV2 : IReportParser<IDeviceReport>
+    {
+        public IDeviceReport Parse(byte[] report)
+        {
+            if (report[1] == 0xC0)
+                return new OutOfRangeReport(report);
+
+            // The tablet throw inits sent its way back to us, we ignore them.
+            if (report[1] == 0xB4)
+                return new DeviceReport(report);
+
+            if (report[1].IsBitSet(4))
+                return new XP_PenAuxReport(report, secondWheelBitOffset: -2);
+
+            if (report.Length >= 12)
+                return new XP_PenTabletOverflowReport(report);
+            else if (report.Length >= 10)
+                return new XP_PenTabletReport(report);
+            else
+                return new TabletReport(report);
+        }
+    }
+}
